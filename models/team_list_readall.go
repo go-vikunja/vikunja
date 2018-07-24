227a2afd37de05d202c0ea5b7af0e475285b82ae
kolaente <konrad@kola-entertainments.de>
2018-07-24 17:29:46 +0200

    implemented viewing all teams of a list

---
+++ models/team_list_readall.go
@@ -0,0 +1,24 @@
+package models
+
+// ReadAll implements the method to read all teams of a namespace
+func (tl *TeamList) ReadAll(user *User) (interface{}, error) {
+	// Check if the user can read the namespace
+	l, err := GetListByID(tl.ListID)
+	if err != nil {
+		return nil, err
+	}
+	if !l.CanRead(user) {
+		return nil, ErrNeedToHaveListReadAccess{ListID: tl.ListID, UserID: user.ID}
+	}
+
+	// Get the teams
+	all := []*Team{}
+
+	err = x.Select("teams.*").
+		Table("teams").
+		Join("INNER", "team_list", "team_id = teams.id").
+		Where("team_list.list_id = ?", tl.ListID).
+		Find(&all)
+
+	return all, err
+}
