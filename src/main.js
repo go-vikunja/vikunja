8ccb1893f159d910a9fc749da7960f257d2e2e5c
kolaente <konrad@kola-entertainments.de>
2018-09-06 19:56:44 +0200

    Properly added bulma

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import auth from './auth'
 
+import '../node_modules/bulma/bulma.sass'
+
 Vue.config.productionTip = false
 
 // Check the user's auth status when the app starts
