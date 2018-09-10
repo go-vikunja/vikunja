b91d9eb71209766195a4bf58c04dd0a252af98a2
kolaente <konrad@kola-entertainments.de>
2018-09-10 07:41:39 +0200

    Fixed task update function to be able to update a task with minimal informaton

--- models/list_items_create_update.go
+++ models/list_items_create_update.go
@@ -2,25 +2,7 @@ package models
 
 // Create is the implementation to create a list task
 func (i *ListTask) Create(doer *User) (err error) {
-	//i.ListID = lID
 	i.ID = 0
-
-	return createOrUpdateListTask(i, doer)
-}
-
-// Update updates a list task
-func (i *ListTask) Update() (err error) {
-	// Check if the task exists
-	_, err = GetListTaskByID(i.ID)
-	if err != nil {
-		return
-	}
-
-	return createOrUpdateListTask(i, &User{})
-}
-
-// Helper function for creation or updating of new lists as both methods share most of their logic
-func createOrUpdateListTask(i *ListTask, doer *User) (err error) {
 
 	// Check if we have at least a text
 	if i.Text == "" {
@@ -33,19 +15,26 @@ func createOrUpdateListTask(i *ListTask, doer *User) (err error) {
 		return
 	}
 
-	// Do the update
-	if i.ID != 0 {
-		_, err = x.ID(i.ID).Update(i)
-	} else {
-		user, err := GetUserByID(doer.ID)
-		if err != nil {
-			return err
-		}
-
-		i.CreatedByID = user.ID
-		i.CreatedBy = user
-		_, err = x.Insert(i)
+	user, err := GetUserByID(doer.ID)
+	if err != nil {
+		return err
 	}
 
-	return
+	i.CreatedByID = user.ID
+	i.CreatedBy = user
+	_, err = x.Insert(i)
+	return err
+}
+
+// Update updates a list task
+func (i *ListTask) Update() (err error) {
+	// Check if the task exists
+	_, err = GetListTaskByID(i.ID)
+	if err != nil {
+		return
+	}
+
+	// Do the update
+	_, err = x.ID(i.ID).Update(i)
+	return err
 }
