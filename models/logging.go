8d27daccc625f4badc06cd398f6fe2da76636482
kolaente <konrad@kola-entertainments.de>
2018-09-20 07:31:36 +0200

    Fixed lint & fmt

--- models/logging.go
+++ models/logging.go
@@ -5,15 +5,16 @@ import (
 	"os"
 )
 
+// Log is the handler for the logger
 var Log = logging.MustGetLogger("vikunja")
 
 var format = logging.MustStringFormatter(
 	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
 )
 
+// InitLogger initializes the global log handler
 func InitLogger() {
 	backend := logging.NewLogBackend(os.Stderr, "", 0)
 	backendFormatter := logging.NewBackendFormatter(backend, format)
 	logging.SetBackend(backendFormatter)
 }
-
