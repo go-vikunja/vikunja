17368dea9a187c2fe35442d017a0bc9fc64ccea7
konrad <konrad@kola-entertainments.de>
2018-10-03 19:00:09 +0200

    Cleanup

--- routes/api/v1/user_show.go
+++ routes/api/v1/user_show.go
@@ -6,25 +6,38 @@ import (
 	"net/http"
 )
 
-// UserShow gets all information about a user
+// UserShow gets all informations about the current user
 func UserShow(c echo.Context) error {
+	// swagger:operation GET /user user showUser
+	// ---
+	// summary: Shows the current user
+	// consumes:
+	// - application/json
+	// produces:
+	// - application/json
+	// responses:
+	//   "200":
+	//     "$ref": "#/responses/User"
+	//   "400":
+	//     "$ref": "#/responses/Message"
+	//   "500":
+	//     "$ref": "#/responses/Message"
+
 	userInfos, err := models.GetCurrentUser(c)
 	if err != nil {
 		if models.IsErrUserDoesNotExist(err) {
-			return c.JSON(http.StatusNotFound, models.Message{"The user does not exist."})
+			return echo.NewHTTPError(http.StatusNotFound, "The user does not exist.")
 		}
-		return c.JSON(http.StatusInternalServerError, models.Message{"Error getting user infos."})
+		return echo.NewHTTPError(http.StatusInternalServerError, "Error getting user infos.")
 	}
 
 	user, err := models.GetUserByID(userInfos.ID)
 	if err != nil {
 		if models.IsErrUserDoesNotExist(err) {
-			return c.JSON(http.StatusNotFound, models.Message{"The user does not exist."})
+			return echo.NewHTTPError(http.StatusNotFound, "The user does not exist.")
 		}
-		return c.JSON(http.StatusInternalServerError, models.Message{"Error getting user infos."})
+		return echo.NewHTTPError(http.StatusInternalServerError, "Error getting user infos.")
 	}
-	// Obfuscate his password
-	user.Password = ""
 
 	return c.JSON(http.StatusOK, user)
 }
