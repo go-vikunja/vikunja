a1a77a9b756c0b09cddf784b9294d7ad60667440
kolaente <konrad@kola-entertainments.de>
2018-10-05 18:49:56 +0200

    improved logging format

--- models/logging.go
+++ models/logging.go
@@ -9,7 +9,7 @@ import (
 var Log = logging.MustGetLogger("vikunja")
 
 var format = logging.MustStringFormatter(
-	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+	`%{color}%{time:2006-01-02 15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
 )
 
 // InitLogger initializes the global log handler
