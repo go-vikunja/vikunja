f4e84d44970dbfb66f326e9b1b80d0be2ef1599c
konrad <konrad@kola-entertainments.de>
2018-11-25 22:20:35 +0000

    Add better looking checkboxes (#6)

--- src/components/lists/ShowList.vue
+++ src/components/lists/ShowList.vue
@@ -30,8 +30,18 @@
 				<div class="box tasks" v-if="this.list.tasks && this.list.tasks.length > 0">
 					<div class="task" v-for="l in list.tasks" v-bind:key="l.id">
 						<label v-bind:for="l.id">
-							<input @change="markAsDone" type="checkbox" v-bind:id="l.id" v-bind:checked="l.done">
-							{{l.text}}
+							<div class="fancycheckbox">
+								<input @change="markAsDone" type="checkbox" v-bind:id="l.id" v-bind:checked="l.done" style="display: none;">
+								<label  v-bind:for="l.id" class="check">
+									<svg width="18px" height="18px" viewBox="0 0 18 18">
+										<path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+										<polyline points="1 9 7 14 15 4"></polyline>
+									</svg>
+								</label>
+							</div>
+							<span class="tasktext">
+								{{l.text}}
+							</span>
 						</label>
 						<div @click="editTask(l.id)" class="icon settings">
 							<icon icon="cog"/>
