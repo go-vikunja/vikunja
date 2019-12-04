e890001ee1a8d0f4a651992dedc7289f57e6135d
shilch <simon@hilchenba.ch>
2019-12-04 19:39:56 +0000

    Consistent copyright text in file headers (#112)

--- pkg/models/error.go
+++ pkg/models/error.go
@@ -1,18 +1,18 @@
-//  Vikunja is a todo-list application to facilitate your life.
-//  Copyright 2018 Vikunja and contributors. All web.Rights reserved.
+// Vikunja is a todo-list application to facilitate your life.
+// Copyright 2018-2019 Vikunja and contributors. All web.Rights reserved.
 //
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 package models
 
@@ -1221,7 +1221,7 @@ func (err ErrUserHasNoAccessToLabel) HTTPError() web.HTTPError {
 }
 
 // ========
-//  Rights
+// Rights
 // ========
 
 // ErrInvalidRight represents an error where a right is invalid
