5e170e14ccaa61a362a5a063dcef6c807be92fd5
kolaente <k@knt.li>
2020-07-05 17:31:15 +0200

    Fix checking for existing migration from other services

--- src/components/migrator/migration.vue
+++ src/components/migrator/migration.vue
@@ -2,11 +2,19 @@
 	<div class="content">
 		<h1>Import your data from {{ name }} to Vikunja</h1>
 		<p>Vikunja will import all lists, tasks, notes, reminders and files you have access to.</p>
-		<template v-if="isMigrating === false && message === '' && lastMigrationDate === 0">
+		<template v-if="isMigrating === false && message === '' && lastMigrationDate === null">
 			<p>To authorize Vikunja to access your {{ name }} Account, click the button below.</p>
-			<a :href="authUrl" class="button is-primary" :class="{'is-loading': migrationService.loading}" :disabled="migrationService.loading">Get Started</a>
+			<a
+					:href="authUrl"
+					class="button is-primary"
+					:class="{'is-loading': migrationService.loading}"
+					:disabled="migrationService.loading">
+				Get Started
+			</a>
 		</template>
-		<div class="migration-in-progress-container" v-else-if="isMigrating === true && message === '' && lastMigrationDate === 0">
+		<div
+				class="migration-in-progress-container"
+				v-else-if="isMigrating === true && message === '' && lastMigrationDate === null">
 			<div class="migration-in-progress">
 				<img :src="`/images/migration/${identifier}.png`" :alt="name"/>
 				<div class="progress-dots">
@@ -46,7 +54,7 @@
 </template>
 
 <script>
-	import AbstractMigrationService from "../../services/migrator/abstractMigrationService";
+	import AbstractMigrationService from '../../services/migrator/abstractMigrationService'
 
 	export default {
 		name: 'migration',
@@ -74,11 +82,11 @@
 			this.getAuthUrl()
 			this.message = ''
 
-			if(typeof this.$route.query.code !== 'undefined') {
+			if (typeof this.$route.query.code !== 'undefined') {
 				this.wunderlistCode = this.$route.query.code
 				this.migrationService.getStatus()
 					.then(r => {
-						if(r.time) {
+						if (r.time) {
 							this.lastMigrationDate = new Date(r.time)
 							return
 						}
