b1c750373e39a675336ad5d7af35e95962cf0b77
konrad <konrad@kola-entertainments.de>
2020-07-07 07:48:12 +0000

    Fix creating lists with non ascii characters (#607)
    Fix test name
    
    Fix migrating lists with non utf-8 characters in their name
    
    Fix creating lists with non utf-8 characters
    
    Co-authored-by: kolaente <k@knt.li>
    Reviewed-on: https://kolaente.dev/vikunja/api/pulls/607
    (cherry picked from commit 4db06ba9a1d7114b844352bde5eee115fdb0b188)

--- pkg/models/list.go
+++ pkg/models/list.go
@@ -404,7 +404,7 @@ func GenerateListIdentifier(l *List, sess *xorm.Engine) (err error) {
 	// The general idea here is to take the title and slice it into pieces, until we found a unique piece.
 
 	var exists = true
-	titleSlug := strings.Replace(strings.ToUpper(l.Title), " ", "", -1)
+	titleSlug := []rune(strings.Replace(strings.ToUpper(l.Title), " ", "", -1))
 
 	// We can save at most 10 characters in the db, so we need to ensure it has at most 10 characters
 	if len(titleSlug) > 10 {
@@ -421,7 +421,7 @@ func GenerateListIdentifier(l *List, sess *xorm.Engine) (err error) {
 		}
 
 		// Take a random part of the title slug, starting at the beginning
-		l.Identifier = titleSlug[i:]
+		l.Identifier = string(titleSlug[i:])
 		exists, err = sess.
 			Where("identifier = ?", l.Identifier).
 			And("id != ?", l.ID).
