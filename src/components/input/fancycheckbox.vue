792a80ab4417e87eaede1bdf66c4306cd22e0d40
kolaente <k@knt.li>
2020-07-07 21:38:38 +0200

    Fix task list if it has tasks with a long unbreakable title

--- src/components/input/fancycheckbox.vue
+++ src/components/input/fancycheckbox.vue
@@ -1,6 +1,12 @@
 <template>
 	<div class="fancycheckbox" :class="{'is-disabled': disabled}">
-		<input @change="updateData" type="checkbox" :id="checkBoxId" :checked="checked" style="display: none;" :disabled="disabled">
+		<input
+				@change="updateData"
+				type="checkbox"
+				:id="checkBoxId"
+				:checked="checked"
+				style="display: none;"
+				:disabled="disabled"/>
 		<label :for="checkBoxId" class="check">
 			<svg width="18px" height="18px" viewBox="0 0 18 18">
 				<path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
