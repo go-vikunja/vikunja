0999f9ae9badbf97fc463a45ef03d6ce804dab41
kolaente <k@knt.li>
2020-10-17 19:59:28 +0200

    Make sure the initial screen size is bigger

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const frontendPath = 'frontend/'
 function createWindow() {
 	// Create the browser window.
 	const mainWindow = new BrowserWindow({
-		width: 800,
-		height: 600,
+		width: 1680,
+		height: 960,
 		webPreferences: {
 			nodeIntegration: true,
 		}
