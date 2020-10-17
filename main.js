75fdd5daf332e8fe052f49525fbaf6be0bec7b55
kolaente <k@knt.li>
2020-10-17 18:16:02 +0200

    Cleanup

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow, shell} = require('electron')
 
 function createWindow() {
 	// Create the browser window.
 	const mainWindow = new BrowserWindow({
 		width: 800,
 		height: 600,
-		// webPreferences: {
-		//   preload: path.join(__dirname, 'preload.js')
-		// }
+		webPreferences: {
+			nodeIntegration: true,
+		}
 	})
 
-	// and load the index.html of the app.
-	mainWindow.loadFile('frontend/index.html')
+	// Open external links in the browser
+	mainWindow.webContents.on('new-window', function (e, url) {
+		e.preventDefault()
+		shell.openExternal(url)
+	})
+
+	// Hide the toolbar
+	mainWindow.setMenuBarVisibility(false)
+
 
 	// Open the DevTools.
-	mainWindow.webContents.openDevTools()
+	// mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -35,9 +40,6 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
