50b1d378e08a97236ba0b5c31848b2253b7ef8ab
kolaente <k@knt.li>
2020-11-12 20:02:40 +0100

    Fix list not added to lists when duplicating

--- src/views/list/EditList.vue
+++ src/views/list/EditList.vue
@@ -269,6 +269,7 @@ export default {
 			this.listDuplicateService.create(listDuplicate)
 				.then(r => {
 					this.$store.commit('namespaces/addListToNamespace', r.list)
+					this.$store.commit('lists/addList', r.list)
 					this.success({message: 'The list was successfully duplicated.'}, this)
 					router.push({name: 'list.index', params: {listId: r.list.id}})
 				})
