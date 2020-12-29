456af4d4a9e812e615e99f465edf141bf8540d3e
kolaente <k@knt.li>
2020-12-29 02:04:20 +0100

    Fix go header lint

--- pkg/integrations/archived_test.go
+++ pkg/integrations/archived_test.go
@@ -1,19 +1,18 @@
-// Copyright 2020 Vikunja and contriubtors. All rights reserved.
+// Vikunja is a to-do list application to facilitate your life.
+// Copyright 2018-2020 Vikunja and contributors. All rights reserved.
 //
-// This file is part of Vikunja.
-//
-// Vikunja is free software: you can redistribute it and/or modify
+// This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public Licensee as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Vikunja is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Affero General Public Licensee for more details.
 //
 // You should have received a copy of the GNU Affero General Public Licensee
-// along with Vikunja.  If not, see <https://www.gnu.org/licenses/>.
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 package integrations
 
--- pkg/routes/api/v1/avatar.go
+++ pkg/routes/api/v1/avatar.go
@@ -1,4 +1,4 @@
-// Vikunja is a to-do-list application to facilitate your life.
+// Vikunja is a to-do list application to facilitate your life.
 // Copyright 2018-2020 Vikunja and contributors. All rights reserved.
 //
 // This program is free software: you can redistribute it and/or modify
