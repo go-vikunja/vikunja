85eeccee285d276507add5168455e762e4887a88
kolaente <k@knt.li>
2021-01-10 22:46:50 +0100

    Inline mutliselect search input for multiple elements

--- src/components/input/multiselect.vue
+++ src/components/input/multiselect.vue
@@ -4,7 +4,7 @@
 		:class="{'has-search-results': searchResultsVisible}"
 		ref="multiselectRoot"
 	>
-		<div class="input-wrapper input">
+		<div class="input-wrapper input" :class="{'has-multiple': multiple}">
 			<template v-if="Array.isArray(internalValue)">
 				<template v-for="(item, key) in internalValue">
 					<slot name="tag" :item="item">
