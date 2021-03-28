bc782e68fffeacc885057b58ab26030d8caecd31
kolaente <k@knt.li>
2021-03-28 18:18:58 +0200

    Add systemd service file to linux packages

--- build/after-install.sh
+++ build/after-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+systemctl enable vikunja.service
+
 # Fix the config to contain proper values
 NEW_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 sed -i "s/<jwt-secret>/$NEW_SECRET/g" /etc/vikunja/config.yml
