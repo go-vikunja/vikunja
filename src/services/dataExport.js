debdc83f1b201cc2ae450bffacdf5d0c21577c3d
kolaente <k@knt.li>
2021-09-08 17:33:58 +0200

    Fix data export download progress

--- src/services/dataExport.js
+++ src/services/dataExport.js
@@ -7,7 +7,9 @@ export default class DataExportService extends AbstractService {
 	}
 	
 	download(password) {
+		const clear = this.setLoading()
 		return this.getBlobUrl('/user/export/download', 'POST', {password})
 			.then(url => downloadBlob(url, 'vikunja-export.zip'))
+			.finally(() => clear())
 	}
 }
