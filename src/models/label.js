87c70cec0e91f03f9bb58a247ef34be849c11a45
dpschen <dpschen@noreply.kolaente.de>
2021-09-08 21:44:56 +0000

    chore: define default label background color once (#713)
    Co-authored-by: Dominik Pschenitschni <mail@celement.de>
    Reviewed-on: https://kolaente.dev/vikunja/frontend/pulls/713
    Co-authored-by: dpschen <dpschen@noreply.kolaente.de>
    Co-committed-by: dpschen <dpschen@noreply.kolaente.de>

--- src/models/label.js
+++ src/models/label.js
@@ -2,12 +2,15 @@ import AbstractModel from './abstractModel'
 import UserModel from './user'
 import {colorIsDark} from '@/helpers/color/colorIsDark'
 
+const DEFAULT_LABEL_BACKGROUND_COLOR = 'e8e8e8'
 export default class LabelModel extends AbstractModel {
 	constructor(data) {
 		super(data)
+		// FIXME: this should be empty and be definied in the client.
+		// that way it get's never send to the server db and is easier to change in future versions.
 		// Set the default color
 		if (this.hexColor === '') {
-			this.hexColor = 'e8e8e8'
+			this.hexColor = DEFAULT_LABEL_BACKGROUND_COLOR
 		}
 		if (this.hexColor.substring(0, 1) !== '#') {
 			this.hexColor = '#' + this.hexColor
