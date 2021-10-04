587f2edd83da9102f4ab0ecd808443f5faaefdd0
kolaente <k@knt.li>
2021-10-04 20:55:05 +0200

    fix(docker): properly replace api url

--- run.sh
+++ run.sh
@@ -12,7 +12,8 @@ echo "Using $VIKUNJA_API_URL as default api url"
 # Escape the variable to prevent sed from complaining
 VIKUNJA_API_URL=$(echo $VIKUNJA_API_URL |sed 's/\//\\\//g')
 
-sed -i "s/http\:\/\/localhost\:3456\/api\/v1/$VIKUNJA_API_URL/g" /usr/share/nginx/html/index.html
+sed -i "s/http\:\/\/localhost\:3456//g" /usr/share/nginx/html/index.html # replacing in two steps to make sure api urls from releases are properly replaced as well
+sed -i "s/'\/api\/v1/'$VIKUNJA_API_URL/g" /usr/share/nginx/html/index.html
 
 sed -i "s/listen 80/listen $VIKUNJA_HTTP_PORT/g" /etc/nginx/nginx.conf
 sed -i "s/listen 443/listen $VIKUNJA_HTTPS_PORT/g" /etc/nginx/nginx.conf
