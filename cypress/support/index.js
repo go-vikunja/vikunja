e6a935f49dca3301f0ef8cb138909f67a4fa72e1
dpschen <dpschen@noreply.kolaente.de>
2021-10-11 18:39:27 +0000

    fix: disable service workers in cypress (#830)
    Co-authored-by: Dominik Pschenitschni <mail@celement.de>
    Reviewed-on: https://kolaente.dev/vikunja/frontend/pulls/830
    Reviewed-by: konrad <k@knt.li>
    Co-authored-by: dpschen <dpschen@noreply.kolaente.de>
    Co-committed-by: dpschen <dpschen@noreply.kolaente.de>

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,3 +2,10 @@
 import './commands'
 import 'cypress-file-upload'
 import '@4tw/cypress-drag-drop'
+
+// see https://github.com/cypress-io/cypress/issues/702#issuecomment-587127275
+Cypress.on('window:before:load', (win) => {
+	// disable service workers
+	// @ts-ignore
+	delete win.navigator.__proto__.ServiceWorker
+})
