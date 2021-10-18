a33758e37ed787585ed116c6abc2d591602cf1d0
Dominik Pschenitschni <mail@celement.de>
2021-10-18 14:18:49 +0200

    feat: moved some card.scss styles to the card.vue component

--- src/components/misc/card.vue
+++ src/components/misc/card.vue
@@ -54,3 +54,25 @@ export default {
 	emits: ['close'],
 }
 </script>
+
+<style lang="scss" scoped>
+.card {
+  background-color: $white;
+  border-radius: $radius;
+  margin-bottom: 1rem;
+  border: 1px solid $grey-200;
+  box-shadow: $shadow-sm;
+}
+
+.card-header {
+  box-shadow: none;
+  border-bottom: 1px solid $grey-200;
+  border-radius: $radius $radius 0 0;
+}
+
+// FIXME: should maybe be merged somehow with modal
+::v-deep.modal-card-foot {
+  background-color: $grey-50;
+  border-top: 0;
+}
+</style>
