1873c747761573d26107a79c26a7e476968a75d2
konrad <k@knt.li>
2021-10-31 13:37:57 +0000

    feat: add releases to sentry (#919)
    Co-authored-by: kolaente <k@knt.li>
    Reviewed-on: https://kolaente.dev/vikunja/frontend/pulls/919
    Reviewed-by: dpschen <dpschen@noreply.kolaente.de>
    Co-authored-by: konrad <k@knt.li>
    Co-committed-by: konrad <k@knt.li>

--- src/sentry.js
+++ src/sentry.js
@@ -1,8 +1,11 @@
+import {VERSION} from './version.json'
+
 export default async function setupSentry(app, router) {
 	const Sentry = await import('@sentry/vue')
 	const {Integrations} = await import('@sentry/tracing')
 
 	Sentry.init({
+		release: VERSION,
 		app,
 		dsn: window.SENTRY_DSN,
 		integrations: [
