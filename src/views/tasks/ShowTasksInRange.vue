108e7af57847c4c96ac53f50d6610b4311d53820
dpschen <dpschen@noreply.kolaente.de>
2021-11-02 18:18:54 +0000

    feat: use script setup for ShowTasksinRange.vue (#931)
    Co-authored-by: Dominik Pschenitschni <mail@celement.de>
    Reviewed-on: https://kolaente.dev/vikunja/frontend/pulls/931
    Reviewed-by: konrad <k@knt.li>
    Co-authored-by: dpschen <dpschen@noreply.kolaente.de>
    Co-committed-by: dpschen <dpschen@noreply.kolaente.de>

--- src/views/tasks/ShowTasksInRange.vue
+++ src/views/tasks/ShowTasksInRange.vue
@@ -7,23 +7,14 @@
 	</div>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue'
 import ShowTasks from './ShowTasks'
 
 function getNextWeekDate() {
 	return new Date((new Date()).getTime() + 7 * 24 * 60 * 60 * 1000)
 }
 
-export default {
-	name: 'ShowTasksInRange',
-	components: {
-		ShowTasks,
-	},
-	data() {
-		return {
-			startDate: new Date(),
-			endDate: getNextWeekDate(),
-		}
-	},
-}
+const startDate = ref(new Date())
+const endDate = ref(getNextWeekDate())
 </script>
