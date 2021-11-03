a47d1069268d6ffda25e0168bcd0a535b67eee6e
kolaente <k@knt.li>
2021-11-03 18:12:40 +0100

    chore: remove weblate ping script

--- ping-weblate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# Shell script because yaml doesn't understand the header is a string literal and not a yaml symbol
-
-curl -d operation=pull -H "Authorization: Token $WEBLATE_TOKEN" https://hosted.weblate.org/api/projects/vikunja/repository/
-curl -d operation=push -H "Authorization: Token $WEBLATE_TOKEN" https://hosted.weblate.org/api/projects/vikunja/repository/
