cf849da104a103454e2f5270cb05201ce5795d3f
Dominik Pschenitschni <dpschen@noreply.kolaente.de>
2022-02-08 20:35:03 +0000

    fix: make logo change reactive (#1509)
    Co-authored-by: Dominik Pschenitschni <mail@celement.de>
    Reviewed-on: https://kolaente.dev/vikunja/frontend/pulls/1509
    Reviewed-by: konrad <k@knt.li>
    Co-authored-by: Dominik Pschenitschni <dpschen@noreply.kolaente.de>
    Co-committed-by: Dominik Pschenitschni <dpschen@noreply.kolaente.de>

--- src/components/home/Logo.vue
+++ src/components/home/Logo.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { computed } from 'vue'
+import { useNow } from '@vueuse/core'
+
 import LogoFull from '@/assets/logo-full.svg?component'
 import LogoFullPride from '@/assets/logo-full-pride.svg?component'
 
-const Logo = computed(() => new Date().getMonth() === 5 ? LogoFullPride : LogoFull)
+const now = useNow()
+const Logo = computed(() => now.value.getMonth() === 5 ? LogoFullPride : LogoFull)
 </script>
 
 <template>
