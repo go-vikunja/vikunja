7fe9f17e43c1d18380dd92da1c88212050a9dc90
Dominik Pschenitschni <mail@celement.de>
2022-02-13 13:49:00 +0100

    feat: setup cypress

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,4 +1,3 @@
-/// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -12,10 +11,21 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+import path from 'path'
+import { startDevServer } from '@cypress/vite-dev-server'
+
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+export default (on, config) => {
+	// `on` is used to hook into various events Cypress emits
+	// `config` is the resolved Cypress config
+  on('dev-server:start', (options) => {
+    return startDevServer({
+      options,
+      viteConfig: {
+        configFile: path.resolve(__dirname, '..', '..', 'vite.config.ts'),
+      },
+    })
+  })
 }
--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,3 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-svg-loader" />
+/// <reference types="cypress" />
