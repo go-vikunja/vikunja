c06cc6ad7aec3f91eb8fdbe9dc621a5b4577b6aa
kolaente <k@knt.li>
2022-02-26 14:48:46 +0100

    fix: pop sound not saved and played when marking tasks done

--- src/helpers/playPop.ts
+++ src/helpers/playPop.ts
@@ -3,11 +3,15 @@ import popSoundFile from '@/assets/audio/pop.mp3'
 export const playSoundWhenDoneKey = 'playSoundWhenTaskDone'
 
 export function playPop() {
-	const enabled = Boolean(localStorage.getItem(playSoundWhenDoneKey))
-	if(!enabled) {
+	const enabled = localStorage.getItem(playSoundWhenDoneKey) === 'true'
+	if (!enabled) {
 		return
 	}
 
+	playPopSound()
+}
+
+export function playPopSound() {
 	const popSound = new Audio(popSoundFile)
 	popSound.play()
 }
