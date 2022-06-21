c6d214b9ebb469a0636c86c5b5b62dc335ac53c7
Dominik Pschenitschni <mail@celement.de>
2022-06-21 23:51:58 +0200

    fix: cypress plugins

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,12 +11,13 @@ export default defineConfig({
   },
   projectId: '181c7x',
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts')(on, config)
-    },
     baseUrl: 'http://localhost:4173',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+	component: {
+    devServer: {
+      framework: 'vue',
+      bundler: 'vite',
+    },
   },
 })
