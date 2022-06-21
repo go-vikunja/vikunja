77466e337353fdc1d7ee5eb7b21c55c752c4b6bd
Dominik Pschenitschni <mail@celement.de>
2022-06-21 23:16:05 +0200

    fix: cypress plugins import

--- cypress.config.ts
+++ cypress.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require('./cypress/plugins/index.ts')(on, config)
     },
     baseUrl: 'http://localhost:4173',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
