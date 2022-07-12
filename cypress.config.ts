97e6147351fb73c7cefa11eb9ec1a3f3076ba2b8
kolaente <k@knt.li>
2022-07-12 09:09:26 +0200

    feat(tests): change cypress default viewport size

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,23 +1,25 @@
-import { defineConfig } from 'cypress'
+import {defineConfig} from 'cypress'
 
 export default defineConfig({
-  env: {
-    API_URL: 'http://localhost:3456/api/v1',
-    TEST_SECRET: 'averyLongSecretToSe33dtheDB',
-  },
-  video: false,
-  retries: {
-    runMode: 2,
-  },
-  projectId: '181c7x',
-  e2e: {
-    baseUrl: 'http://localhost:4173',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-  },
+	env: {
+		API_URL: 'http://localhost:3456/api/v1',
+		TEST_SECRET: 'averyLongSecretToSe33dtheDB',
+	},
+	video: false,
+	retries: {
+		runMode: 2,
+	},
+	projectId: '181c7x',
+	e2e: {
+		baseUrl: 'http://localhost:4173',
+		specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+	},
 	component: {
-    devServer: {
-      framework: 'vue',
-      bundler: 'vite',
-    },
-  },
+		devServer: {
+			framework: 'vue',
+			bundler: 'vite',
+		},
+	},
+	viewportWidth: 1600,
+	viewportHeight: 900,
 })
