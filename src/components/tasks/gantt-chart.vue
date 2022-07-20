736e5a8bf55ccf7cbed23fd3af48122c459bcdc6
kolaente <k@knt.li>
2022-07-20 18:44:02 +0200

    feat: dynamically set default date

--- src/components/tasks/gantt-chart.vue
+++ src/components/tasks/gantt-chart.vue
@@ -51,8 +51,8 @@ const ganttBars = ref([])
 // We need a "real" ref object for the gantt bars to instantly update the tasks when they are dragged on the chart.
 // A computed won't work directly.
 function mapGanttBars() {
-	const defaultStartDate = '2022-07-19 12:00'
-	const defaultEndDate = '2022-07-25 12:00'
+	const defaultStartDate = format(new Date(), dateFormat)
+	const defaultEndDate = format(new Date((new Date()).setDate((new Date()).getDate() + 7)), dateFormat)
 
 	tasks.value.forEach(t => ganttBars.value.push([{
 		startDate: t.startDate ? format(t.startDate, dateFormat) : defaultStartDate,
