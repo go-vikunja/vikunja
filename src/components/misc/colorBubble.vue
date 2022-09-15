2683fec0a67f6afd16579bb44a6ceadc0edd565f
kolaente <k@knt.li>
2022-09-15 13:56:14 +0200

    feat: show the task color bubble everywhere

---
+++ src/components/misc/colorBubble.vue
@@ -0,0 +1,26 @@
+<template>
+	<span
+		:style="{backgroundColor: color }"
+		class="color-bubble"
+	></span>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+	color: {
+		type: String,
+		required: true,
+	},
+})
+</script>
+
+<style scoped>
+.color-bubble {
+	display: inline-block;
+	border-radius: 100%;
+	margin-right: 4px;
+	height: 10px;
+	width: 10px;
+	flex-shrink: 0;
+}
+</style>
