8ce242bb6595ef12442a6ba0fb37eb66c65dd71b
kolaente <k@knt.li>
2022-09-29 18:08:05 +0200

    chore: use better variable names

--- src/helpers/parseSubtasksViaIndention.ts
+++ src/helpers/parseSubtasksViaIndention.ts
@@ -14,26 +14,26 @@ const spaceRegex = /^ */
 export function parseSubtasksViaIndention(taskTitles: string): TaskWithParent[] {
 	const titles = taskTitles.split(/[\r\n]+/)
 
-	return titles.map((t, i) => {
+	return titles.map((title, index) => {
 		const task: TaskWithParent = {
-			title: cleanupTitle(t),
+			title: cleanupTitle(title),
 			parent: null,
 		}
 
-		const matched = spaceRegex.exec(t)
+		const matched = spaceRegex.exec(title)
 		const matchedSpaces = matched ? matched[0].length : 0
 
-		if (matchedSpaces > 0 && i > 0) {
+		if (matchedSpaces > 0 && index > 0) {
 			// Go up the tree to find the first task with less indention than the current one
 			let pi = 1
 			let parentSpaces = 0
 			do {
-				task.parent = cleanupTitle(titles[i - pi])
+				task.parent = cleanupTitle(titles[index - pi])
 				pi++
 				const parentMatched = spaceRegex.exec(task.parent)
 				parentSpaces = parentMatched ? parentMatched[0].length : 0
 			} while (parentSpaces >= matchedSpaces)
-			task.title = cleanupTitle(t.replace(spaceRegex, ''))
+			task.title = cleanupTitle(title.replace(spaceRegex, ''))
 			task.parent = task.parent.replace(spaceRegex, '')
 		}
 
