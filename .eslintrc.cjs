362be53a473595ed7e20841cbd9105a1f5459be3
Dominik Pschenitschni <mail@celement.de>
2022-10-03 21:36:55 +0200

    feat: use recommended vue-linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution")
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
 	'root': true,
@@ -7,52 +7,53 @@ module.exports = {
 		'browser': true,
 		'es2022': true,
 		'node': true,
-		'vue/setup-compiler-macros': true,
 	},
 	'extends': [
 		'eslint:recommended',
-		'plugin:vue/vue3-essential',
+		'plugin:vue/vue3-recommended',
 		'@vue/eslint-config-typescript/recommended',
 	],
 	'rules': {
-		'vue/html-quotes': [
-			'error',
-			'double',
-		],
-		'quotes': [
-			'error',
-			'single',
-		],
-		'comma-dangle': [
-			'error',
-			'always-multiline',
-		],
-		'semi': [
-			'error',
-			'never',
-		],
+		'quotes': ['error', 'single'],
+		'comma-dangle': ['error', 'always-multiline'],
+		'semi': ['error', 'never'],
 
-		// see https://segmentfault.com/q/1010000040813116/a-1020000041134455 (original in chinese)
-		'no-unused-vars': 'off',
-		'@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
+		'vue/v-on-event-hyphenation': ['warn', 'never', { 'autofix': true }],
+		'vue/multi-word-component-names': 'warn',
 
-		'vue/multi-word-component-names': 0,
-		// disabled until we have support for reactivityTransform
-		// See https://github.com/vuejs/eslint-plugin-vue/issues/1948
-		// see also setting in `vite.config`
-		'vue/no-setup-props-destructure': 0,
+		// uncategorized rules:
+		'vue/component-api-style': ['error', ['script-setup']],
+		'vue/component-name-in-template-casing': ['warn', 'PascalCase'],
+		'vue/custom-event-name-casing': ['error', 'camelCase'],
+		'vue/define-macros-order': 'error',
+		'vue/match-component-file-name': ['error', {
+			'extensions': ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+			'shouldMatchCase': true,
+		}],
+		'vue/no-boolean-default': ['warn', 'default-false'],
+		'vue/match-component-import-name': 'error',
+		'vue/prefer-separate-static-class': 'warn',
+
+		'vue/padding-line-between-blocks': 'error',
+		'vue/next-tick-style': ['error', 'promise'],
+		'vue/block-lang': [
+			'error',
+			{ 'script': { 'lang': 'ts' } },
+		],
+		'vue/no-required-prop-with-default': ['error', { 'autofix': true }],
+		'vue/no-duplicate-attr-inheritance': 'error',
+		'vue/no-empty-component-block': 'error',
+
+		// vue3
+		'vue/no-ref-object-destructure': 'error',
 	},
 	'parser': 'vue-eslint-parser',
 	'parserOptions': {
 		'parser': '@typescript-eslint/parser',
-		'ecmaVersion': 2022,
-		'sourceType': 'module',
+		'ecmaVersion': 'latest',
 	},
 	'ignorePatterns': [
 		'*.test.*',
 		'cypress/*',
 	],
-	'globals': {
-		'defineProps': 'readonly',
-	},
 }
