5ddce387fe589c574adf0cce438732faf4ad9fd1
kolaente <k@knt.li>
2022-10-24 15:41:12 +0200

    fix: show frontend version in about dialog

--- src/views/About.vue
+++ src/views/About.vue
@@ -34,9 +34,10 @@
 <script setup lang="ts">
 import {computed} from 'vue'
 
-import {VERSION as frontendVersion} from '@/version.json'
+import {VERSION} from '@/version.json'
 import {useConfigStore} from '@/stores/config'
 
 const configStore = useConfigStore()
 const apiVersion = computed(() => configStore.version)
+const frontendVersion = VERSION
 </script>
