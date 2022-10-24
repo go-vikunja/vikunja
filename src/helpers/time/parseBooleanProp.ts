8dea4082bb0766297f74acef0352f8a6a6168d3c
Dominik Pschenitschni <mail@celement.de>
2022-10-24 15:40:39 +0200

    fix: parseBooleanProp

--- src/helpers/time/parseBooleanProp.ts
+++ src/helpers/time/parseBooleanProp.ts
@@ -1,5 +1,5 @@
-export function parseBooleanProp(booleanProp: string) {
+export function parseBooleanProp(booleanProp: string | undefined) {
 	return (booleanProp === 'false' || booleanProp === '0')
 		? false
-		:	Boolean(booleanProp)
+		: Boolean(booleanProp)
 }
