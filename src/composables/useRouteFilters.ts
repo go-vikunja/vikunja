c2dd18edaa8ac29446845a5028d1a04c1f39fc76
kolaente <k@knt.li>
2022-11-09 14:27:26 +0100

    fix: lint & formatting

--- src/composables/useRouteFilters.ts
+++ src/composables/useRouteFilters.ts
@@ -1,16 +1,15 @@
 import {computed, ref, watch, type Ref} from 'vue'
 import {useRouter, type RouteLocationNormalized, type RouteLocationRaw} from 'vue-router'
-import cloneDeep from 'lodash.clonedeep'
 import equal from 'fast-deep-equal/es6'
 
 export type Filters = Record<string, any>
 
 export function useRouteFilters<CurrentFilters extends Filters>(
-		route: Ref<RouteLocationNormalized>,
-		getDefaultFilters: (route: RouteLocationNormalized) => CurrentFilters,
-		routeToFilters: (route: RouteLocationNormalized) => CurrentFilters,
-		filtersToRoute: (filters: CurrentFilters) => RouteLocationRaw,
-	) {
+	route: Ref<RouteLocationNormalized>,
+	getDefaultFilters: (route: RouteLocationNormalized) => CurrentFilters,
+	routeToFilters: (route: RouteLocationNormalized) => CurrentFilters,
+	filtersToRoute: (filters: CurrentFilters) => RouteLocationRaw,
+) {
 	const router = useRouter()
 
 	const filters = ref<CurrentFilters>(routeToFilters(route.value))
@@ -22,22 +21,22 @@ export function useRouteFilters<CurrentFilters extends Filters>(
 			(route && oldRoute && typeof route.name !== 'undefined' && typeof oldRoute.name !== 'undefined' && route.name !== oldRoute.name) ||
 			routeFromFiltersFullPath.value === route.fullPath
 		) {
-      return
-    }
-  
-    filters.value = routeToFilters(route)
-  })
+			return
+		}
 
-  watch(
-    filters,
-    async () => {
-      if (routeFromFiltersFullPath.value !== route.value.fullPath) {
-        await router.push(routeFromFiltersFullPath.value)
-      }
-    },
-    // only apply new route after all filters have changed in component cycle
-    {flush: 'post'},
-  )
+		filters.value = routeToFilters(route)
+	})
+
+	watch(
+		filters,
+		async () => {
+			if (routeFromFiltersFullPath.value !== route.value.fullPath) {
+				await router.push(routeFromFiltersFullPath.value)
+			}
+		},
+		// only apply new route after all filters have changed in component cycle
+		{flush: 'post'},
+	)
 
 	const hasDefaultFilters = computed(() => {
 		return equal(filters.value, getDefaultFilters(route.value))
@@ -47,9 +46,9 @@ export function useRouteFilters<CurrentFilters extends Filters>(
 		filters.value = getDefaultFilters(route.value)
 	}
 
-  return {
-    filters,
-    hasDefaultFilters,
-    setDefaultFilters,
-  }
+	return {
+		filters,
+		hasDefaultFilters,
+		setDefaultFilters,
+	}
 }
