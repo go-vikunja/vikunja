02de481297502ad4b0b2eb2fa3e06366cce6d630
Dominik Pschenitschni <dpschen@noreply.kolaente.de>
2022-11-12 10:48:52 +0000

    feat: use img for logo so that it's not part of the main bundle (#2684)
    Co-authored-by: Dominik Pschenitschni <mail@celement.de>
    Reviewed-on: https://kolaente.dev/vikunja/frontend/pulls/2684
    Co-authored-by: Dominik Pschenitschni <dpschen@noreply.kolaente.de>
    Co-committed-by: Dominik Pschenitschni <dpschen@noreply.kolaente.de>

--- src/components/home/Logo.vue
+++ src/components/home/Logo.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { useNow } from '@vueuse/core'
+import {computed} from 'vue'
+import {useNow} from '@vueuse/core'
 
-import LogoFull from '@/assets/logo-full.svg?component'
-import LogoFullPride from '@/assets/logo-full-pride.svg?component'
+import LogoFull from '@/assets/logo-full.svg?url'
+import LogoFullPride from '@/assets/logo-full-pride.svg?url'
 
 const now = useNow()
-const Logo = computed(() => now.value.getMonth() === 5 ? LogoFullPride : LogoFull)
+const logoUrl = computed(() => now.value.getMonth() === 5 ? LogoFullPride : LogoFull)
 </script>
 
 <template>
-	<Logo alt="Vikunja" class="logo" />
+	<img alt="Vikunja" :src="logoUrl" class="logo" />
 </template>
 
 <style lang="scss" scoped>
