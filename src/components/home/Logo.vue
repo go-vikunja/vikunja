9b050846a41a3d05872cf3ca3ca1f06688084501
kolaente <k@knt.li>
2022-12-10 22:21:44 +0100

    Revert "feat: use img for logo so that it's not part of the main bundle (#2684)"
    This reverts commit 02de481297502ad4b0b2eb2fa3e06366cce6d630.

--- src/components/home/Logo.vue
+++ src/components/home/Logo.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import {computed} from 'vue'
-import {useNow} from '@vueuse/core'
+import { computed } from 'vue'
+import { useNow } from '@vueuse/core'
 
-import LogoFull from '@/assets/logo-full.svg?url'
-import LogoFullPride from '@/assets/logo-full-pride.svg?url'
+import LogoFull from '@/assets/logo-full.svg?component'
+import LogoFullPride from '@/assets/logo-full-pride.svg?component'
 
 const now = useNow()
-const logoUrl = computed(() => now.value.getMonth() === 5 ? LogoFullPride : LogoFull)
+const Logo = computed(() => now.value.getMonth() === 5 ? LogoFullPride : LogoFull)
 </script>
 
 <template>
-	<img alt="Vikunja" :src="logoUrl" class="logo" />
+	<Logo alt="Vikunja" class="logo" />
 </template>
 
 <style lang="scss" scoped>
