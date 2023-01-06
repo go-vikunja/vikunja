72e80f637d7f3b92d3a0ac7f76c4f2ec15eb85cc
Dominik Pschenitschni <dpschen@noreply.kolaente.de>
2023-01-06 09:27:55 +0000

    feat(postcss): mock plugin types (#2930)
    Co-authored-by: Dominik Pschenitschni <mail@celement.de>
    Reviewed-on: https://kolaente.dev/vikunja/frontend/pulls/2930
    Co-authored-by: Dominik Pschenitschni <dpschen@noreply.kolaente.de>
    Co-committed-by: Dominik Pschenitschni <dpschen@noreply.kolaente.de>

---
+++ env.config.d.ts
@@ -0,0 +1,9 @@
+declare module 'postcss-easings' {
+  import postcssEasings from 'postcss-easings'
+  export default postcssEasings
+}
+
+declare module 'postcss-easing-gradients' {
+  import postcssEasingGradients from 'postcss-easing-gradients'
+  export default postcssEasingGradients
+}
