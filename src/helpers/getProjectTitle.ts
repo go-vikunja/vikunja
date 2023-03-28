6f1baa3219093147842efe10f92482364516c84c
kolaente <k@knt.li>
2023-03-28 15:42:02 +0200

    chore: use long variable name

--- src/helpers/getProjectTitle.ts
+++ src/helpers/getProjectTitle.ts
@@ -1,14 +1,14 @@
 import {i18n} from '@/i18n'
 import type {IProject} from '@/modelTypes/IProject'
 
-export function getProjectTitle(p: IProject) {
-	if (p.id === -1) {
+export function getProjectTitle(project: IProject) {
+	if (project.id === -1) {
 		return i18n.global.t('project.pseudo.favorites.title')
 	}
 	
-	if (p.title === 'Inbox') {
+	if (project.title === 'Inbox') {
 		return i18n.global.t('project.inboxTitle')
 	}
 	
-	return p.title
+	return project.title
 }
