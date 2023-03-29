799c0be8306cfc5150611153c59701e96d56893a
kolaente <k@knt.li>
2023-03-29 16:19:58 +0200

    feat: allow selecting a parent project when duplicating a project

--- src/modelTypes/IProjectDuplicate.ts
+++ src/modelTypes/IProjectDuplicate.ts
@@ -4,4 +4,5 @@ import type {IProject} from './IProject'
 export interface IProjectDuplicate extends IAbstract {
 	projectId: number
 	project: IProject
+	parentProjectId: number
 }
--- src/views/project/settings/duplicate.vue
+++ src/views/project/settings/duplicate.vue
@@ -7,23 +7,25 @@
 		:loading="projectDuplicateService.loading"
 	>
 		<p>{{ $t('project.duplicate.text') }}</p>
+		<project-search v-model="parentProject"/>
 	</create-edit>
 </template>
 
 <script setup lang="ts">
-import {ref, shallowReactive} from 'vue'
+import {ref, shallowReactive, watch} from 'vue'
 import {useRoute, useRouter} from 'vue-router'
 import {useI18n} from 'vue-i18n'
 
 import ProjectDuplicateService from '@/services/projectDuplicateService'
 import CreateEdit from '@/components/misc/create-edit.vue'
-import Multiselect from '@/components/input/multiselect.vue'
+import ProjectSearch from '@/components/tasks/partials/projectSearch.vue'
 
 import ProjectDuplicateModel from '@/models/projectDuplicateModel'
 
 import {success} from '@/message'
 import {useTitle} from '@/composables/useTitle'
 import {useProjectStore} from '@/stores/projects'
+import type {IProject} from '@/modelTypes/IProject'
 
 const {t} = useI18n({useScope: 'global'})
 useTitle(() => t('project.duplicate.title'))
@@ -33,11 +35,23 @@ const router = useRouter()
 const projectStore = useProjectStore()
 
 const projectDuplicateService = shallowReactive(new ProjectDuplicateService())
+const parentProject = ref<IProject | null>(null)
+watch(
+	() => route.params.projectId,
+	projectId => {
+		const project = projectStore.getProjectById(route.params.projectId)
+		if (project.parentProjectId) {
+			parentProject.value = projectStore.getProjectById(project.parentProjectId)
+		}
+	},
+	{immediate: true},
+)
 
 async function duplicateProject() {
 	const projectDuplicate = new ProjectDuplicateModel({
 		// FIXME: should be parameter
 		projectId: route.params.projectId,
+		parentProjectId: parentProject.value.id,
 	})
 
 	const duplicate = await projectDuplicateService.create(projectDuplicate)
