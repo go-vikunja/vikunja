929d4f402342de309dd8e453252d22fcb9f362a6
kolaente <k@knt.li>
2023-06-18 18:58:57 +0200

    chore: catch error when trying to play pop sound
    Safari does not allow playing sound without user interaction, so we'll just silently catch and ignore the error until we have a better solution.

--- src/helpers/playPop.ts
+++ src/helpers/playPop.ts
@@ -3,6 +3,10 @@ import popSoundFile from '@/assets/audio/pop.mp3'
 export const playSoundWhenDoneKey = 'playSoundWhenTaskDone'
 
 export function playPopSound() {
-	const popSound = new Audio(popSoundFile)
-	popSound.play()
+	try {
+		const popSound = new Audio(popSoundFile)
+		popSound.play()
+	} catch (e) {
+		console.error('Could not play pop sound:', e)
+	}
 }
