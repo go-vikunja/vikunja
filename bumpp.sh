51376d05dee332f1717fc01c3e7d8b1a61ee7773
kolaente <k@knt.li>
2023-09-04 22:27:04 +0200

    fix: read frontend version from release zip

--- bumpp.sh
+++ bumpp.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-set -e
+set -xe
 
-sed -i "s/\${version}/$VERSION/g" package.json
+frontend_version=$(sed -n 's/.*"VERSION": "\([^"]*\)".*/\1/p' ./frontend/version.json)
 
-sed -i "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" package.json
+sed -i "s/\${version}/$frontend_version/g" package.json
+
+sed -i "s/\"version\": \".*\"/\"version\": \"$frontend_version\"/" package.json
 
