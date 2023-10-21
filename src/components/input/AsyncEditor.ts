d6a41fa518fd981f7f0cc618d8f362a9d225a9a3
kolaente <k@knt.li>
2023-10-21 19:48:17 +0200

    chore(editor): remove old editor component

--- src/components/input/AsyncEditor.ts
+++ src/components/input/AsyncEditor.ts
@@ -1,6 +1,5 @@
 import {createAsyncComponent} from '@/helpers/createAsyncComponent'
 
-// const Editor = createAsyncComponent(() => import('@/components/input/editor.vue'))
 const TipTap = createAsyncComponent(() => import('@/components/input/editor/TipTap.vue'))
 
 export default TipTap
