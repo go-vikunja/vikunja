f137064ea9ad7d03454fb85b06ac0f91d8c67f3f
kolaente <k@knt.li>
2024-02-17 16:33:55 +0100

    chore(filters): add histoire story file

---
+++ frontend/src/components/input/FilterInput.story.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import FilterInput from '@/components/project/partials/FilterInput.vue'
+import {ref} from "vue";
+
+function initState(value: string) {
+	return {
+		value,
+	}
+}
+
+const value = ref('')
+</script>
+
+<template>
+	<Story title="Filter Input">
+		<FilterInput v-model="value"/>
+	</Story>
+</template>
