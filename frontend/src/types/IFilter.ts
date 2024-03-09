4e26fa0b85f9f009b621c92d7ee4d82c41be85f3
kolaente <k@knt.li>
2024-03-09 18:45:51 +0100

    fix(filters): correctly use filter in saved filter

--- frontend/src/types/IFilter.ts
+++
@@ -1,9 +0,0 @@
-export interface IFilter {
-	sortBy: ('done' | 'id')[]
-	orderBy: ('asc' | 'desc')[]
-	filterBy: 'done'[]
-	filterValue: 'false'[]
-	filterComparator: 'equals'[]
-	filterConcat: 'and'
-	filterIncludeNulls: boolean
-}
