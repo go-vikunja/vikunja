eb4f880c64e9a4a3e2eb521144630e4901af09e4
kolaente <k@knt.li>
2024-03-12 21:30:54 +0100

    fix(filter): do not show filter footer when creating a filter

--- frontend/src/views/filters/FilterNew.vue
+++ frontend/src/views/filters/FilterNew.vue
@@ -62,6 +62,7 @@
 						:class="{ 'disabled': filterService.loading}"
 						:disabled="filterService.loading"
 						class="has-no-shadow has-no-border"
+						:has-footer="false"
 					/>
 				</div>
 			</div>
