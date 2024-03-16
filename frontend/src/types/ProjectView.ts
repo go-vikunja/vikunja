a3714c74fdcea8ddfac1aff7a3b2262d855177d2
kolaente <k@knt.li>
2024-03-16 12:31:10 +0100

    feat(views): load views when navigating with link share

--- frontend/src/types/ProjectView.ts
+++
@@ -1,8 +0,0 @@
-export const PROJECT_VIEWS = {
-	LIST: 'list',
-	GANTT: 'gantt',
-	TABLE: 'table',
-	KANBAN: 'kanban',
-} as const
-
-export type ProjectView = typeof PROJECT_VIEWS[keyof typeof PROJECT_VIEWS]
