1910f69392dd57236f59d907c61ef77204d1dcd3
kolaente <k@knt.li>
2024-04-06 10:34:41 +0200

    fix(test): correctly mock localstorage in unit tests

--- frontend/src/modules/projectHistory.test.ts
+++ frontend/src/modules/projectHistory.test.ts
@@ -2,14 +2,14 @@ import {test, expect, vi} from 'vitest'
 import {getHistory, removeProjectFromHistory, saveProjectToHistory} from './projectHistory'
 
 test('return an empty history when none was saved', () => {
-	Storage.prototype.getItem = vi.fn(() => null)
+	vi.spyOn(localStorage, 'getItem').mockImplementation(() => null)
 	const h = getHistory()
 	expect(h).toStrictEqual([])
 })
 
 test('return a saved history', () => {
 	const saved = [{id: 1}, {id: 2}]
-	Storage.prototype.getItem = vi.fn(() => JSON.stringify(saved))
+	vi.spyOn(localStorage, 'getItem').mockImplementation(() => JSON.stringify(saved))
 
 	const h = getHistory()
 	expect(h).toStrictEqual(saved)
@@ -17,8 +17,8 @@ test('return a saved history', () => {
 
 test('store project in history', () => {
 	let saved = {}
-	Storage.prototype.getItem = vi.fn(() => null)
-	Storage.prototype.setItem = vi.fn((key, projects) => {
+	vi.spyOn(localStorage, 'getItem').mockImplementation(() => null)
+	vi.spyOn(localStorage, 'setItem').mockImplementation((key: string, projects: string) => {
 		saved = projects
 	})
 
@@ -28,8 +28,8 @@ test('store project in history', () => {
 
 test('store only the last 5 projects in history', () => {
 	let saved: string | null = null
-	Storage.prototype.getItem = vi.fn(() => saved)
-	Storage.prototype.setItem = vi.fn((key: string, projects: string) => {
+	vi.spyOn(localStorage, 'getItem').mockImplementation(() => saved)
+	vi.spyOn(localStorage, 'setItem').mockImplementation((key: string, projects: string) => {
 		saved = projects
 	})
 
@@ -44,8 +44,8 @@ test('store only the last 5 projects in history', () => {
 
 test('don\'t store the same project twice', () => {
 	let saved: string | null = null
-	Storage.prototype.getItem = vi.fn(() => saved)
-	Storage.prototype.setItem = vi.fn((key: string, projects: string) => {
+	vi.spyOn(localStorage, 'getItem').mockImplementation(() => saved)
+	vi.spyOn(localStorage, 'setItem').mockImplementation((key: string, projects: string) => {
 		saved = projects
 	})
 
@@ -56,8 +56,8 @@ test('don\'t store the same project twice', () => {
 
 test('move a project to the beginning when storing it multiple times', () => {
 	let saved: string | null = null
-	Storage.prototype.getItem = vi.fn(() => saved)
-	Storage.prototype.setItem = vi.fn((key: string, projects: string) => {
+	vi.spyOn(localStorage, 'getItem').mockImplementation(() => saved)
+	vi.spyOn(localStorage, 'setItem').mockImplementation((key: string, projects: string) => {
 		saved = projects
 	})
 
@@ -69,11 +69,11 @@ test('move a project to the beginning when storing it multiple times', () => {
 
 test('remove project from history', () => {
 	let saved: string | null = '[{"id": 1}]'
-	Storage.prototype.getItem = vi.fn(() => null)
-	Storage.prototype.setItem = vi.fn((key: string, projects: string) => {
+	vi.spyOn(localStorage, 'getItem').mockImplementation(() => saved)
+	vi.spyOn(localStorage, 'setItem').mockImplementation((key: string, projects: string) => {
 		saved = projects
 	})
-	Storage.prototype.removeItem = vi.fn((key: string) => {
+	vi.spyOn(localStorage, 'removeItem').mockImplementation((key: string) => {
 		saved = null
 	})
 
