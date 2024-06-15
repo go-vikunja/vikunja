baaf8d8f0e244528b13207123fbb6216037e3071
Dominik Pschenitschni <mail@celement.de>
2024-06-15 22:57:35 +0200

    feat(components): align component name casing KanbanCard

--- frontend/src/components/project/views/ProjectKanban.vue
+++ frontend/src/components/project/views/ProjectKanban.vue
@@ -285,7 +285,7 @@ import {useKanbanStore} from '@/stores/kanban'
 
 import ProjectWrapper from '@/components/project/ProjectWrapper.vue'
 import FilterPopup from '@/components/project/partials/FilterPopup.vue'
-import KanbanCard from '@/components/tasks/partials/kanban-card.vue'
+import KanbanCard from '@/components/tasks/partials/KanbanCard.vue'
 import Dropdown from '@/components/misc/Dropdown.vue'
 import DropdownItem from '@/components/misc/DropdownItem.vue'
 
