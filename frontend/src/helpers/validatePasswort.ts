eb95caf757c05f61d8bc705df62e595903e509d5
kolaente <k@knt.li>
2024-09-10 17:44:52 +0200

    fix(password): validate password before sending request to api

---
+++ frontend/src/helpers/validatePasswort.ts
@@ -0,0 +1,15 @@
+export function validatePassword(password: string, validateMinLength: boolean = true): string | true {
+	if (password === '') {
+		return 'user.auth.passwordRequired'
+	}
+
+	if (validateMinLength && password.length < 8) {
+		return 'user.auth.passwordNotMin'
+	}
+	
+	if (validateMinLength && password.length > 250) {
+		return 'user.auth.passwordNotMax'
+	}
+	
+	return true
+}
