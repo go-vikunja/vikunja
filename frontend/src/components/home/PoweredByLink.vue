f3e77eb1f076489617fc8d1bc9e56418a61ad4e1
Dominik Pschenitschni <mail@celement.de>
2024-11-01 17:13:36 +0100

    fix: remove defineProps

--- frontend/src/components/home/PoweredByLink.vue
+++ frontend/src/components/home/PoweredByLink.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script setup lang="ts">
-import {computed, defineProps} from 'vue'
+import {computed} from 'vue'
 import BaseButton from '@/components/base/BaseButton.vue'
 import {POWERED_BY as poweredByUrl} from '@/urls'
 
