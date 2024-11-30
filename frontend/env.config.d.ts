c52b7efc970e88a2bb8009da90118e5fc3c669a3
Dominik Pschenitschni <mail@celement.de>
2024-11-30 18:26:57 +0100

    fix: remove postcss-easings type

--- frontend/env.config.d.ts
+++ frontend/env.config.d.ts
@@ -1,8 +1,3 @@
-declare module 'postcss-easings' {
-  import postcssEasings from 'postcss-easings'
-  export default postcssEasings
-}
-
 declare module 'postcss-easing-gradients' {
   import postcssEasingGradients from 'postcss-easing-gradients'
   export default postcssEasingGradients
