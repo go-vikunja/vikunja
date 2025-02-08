f758312fb65b291d2a957d98cda7efdb42a26719
kolaente <k@knt.li>
2025-02-08 19:08:25 +0100

    fix(ci): move replacing version in desktop release to action

--- desktop/bumpp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -xe
-
-frontend_version=$(git describe --tags --always --abbrev=10)
-
-sed -i "s/\${version}/$frontend_version/g" package.json
-
-sed -i "s/\"version\": \".*\"/\"version\": \"$frontend_version\"/" package.json
