e6d8a0a81231acaccf6e37c64d54bb42309a8ea8
kolaente <k@knt.li>
2025-05-27 17:46:24 +0200

    formatting

--- frontend/src/components/input/filter/schema.ts
+++ frontend/src/components/input/filter/schema.ts
@@ -4,35 +4,35 @@ import { Schema } from '@tiptap/pm/model'
 export const schema = new Schema({
 	nodes: {
 		doc: {
-			content: 'paragraph+'
+			content: 'paragraph+',
 		},
 		paragraph: {
 			content: 'text*',
-			toDOM() { return ['p', 0] }
+			toDOM() { return ['p', 0] },
 		},
-		text: {}
+		text: {},
 	},
 	marks: {
 		// Marks for different parts of the filter expression
 		field: {
 			toDOM() { return ['span', { class: 'field' }, 0] },
-			parseDOM: [{ tag: 'span.field' }]
+			parseDOM: [{ tag: 'span.field' }],
 		},
 		operator: {
 			toDOM() { return ['span', { class: 'operator' }, 0] },
-			parseDOM: [{ tag: 'span.operator' }]
+			parseDOM: [{ tag: 'span.operator' }],
 		},
 		value: {
 			toDOM() { return ['span', { class: 'value' }, 0] },
-			parseDOM: [{ tag: 'span.value' }]
+			parseDOM: [{ tag: 'span.value' }],
 		},
 		logical: {
 			toDOM() { return ['span', { class: 'logical' }, 0] },
-			parseDOM: [{ tag: 'span.logical' }]
+			parseDOM: [{ tag: 'span.logical' }],
 		},
 		grouping: {
 			toDOM() { return ['span', { class: 'grouping' }, 0] },
-			parseDOM: [{ tag: 'span.grouping' }]
-		}
-	}
-});
+			parseDOM: [{ tag: 'span.grouping' }],
+		},
+	},
+})
