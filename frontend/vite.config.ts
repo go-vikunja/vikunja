e2b755c008988b4d79cc2c24ef9c33ade56ecb3f
kolaente <k@knt.li>
2025-06-04 17:21:07 +0200

    chore: upgrade tailwind to v4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ import viteSentry, {type ViteSentryPluginOptions} from 'vite-plugin-sentry'
 import svgLoader from 'vite-svg-loader'
 import postcssPresetEnv from 'postcss-preset-env'
 import postcssEasingGradients from 'postcss-easing-gradients'
-import tailwindcss from 'tailwindcss'
+import tailwindcss from '@tailwindcss/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 const pathSrc = fileURLToPath(new URL('./src', import.meta.url)).replaceAll('\\', '/')
@@ -104,15 +104,15 @@ function getBuildConfig(env: Record<string, string>) {
 				},
 			},
 			postcss: {
-				plugins: [
-					tailwindcss(),
-					postcssEasingGradients(),
-					postcssPresetEnv(),
-				],
+                                plugins: [
+                                        postcssEasingGradients(),
+                                        postcssPresetEnv(),
+                                ],
 			},
 		},
-		plugins: [
-			vue(),
+                plugins: [
+                        tailwindcss(),
+                        vue(),
 			svgLoader({
 				// Since the svgs are already manually optimized via https://jakearchibald.github.io/svgomg/
 				// we don't need to optimize them again.
