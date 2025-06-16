fc369fb55835ac34309d109446e8241b5e8167eb
Dominik Pschenitschni <6173598+dpschen@users.noreply.github.com>
2025-06-16 22:32:40 +0200

    feat(ts): improve module declarations (#957)

--- frontend/env.config.d.ts
+++ frontend/env.config.d.ts
@@ -1,1 +1,3 @@
-declare module 'postcss-easing-gradients';
+declare module 'postcss-easing-gradients'
+declare module 'vite-plugin-sentry/client'
+declare module 'virtual:vite-plugin-sentry/sentry-config'
