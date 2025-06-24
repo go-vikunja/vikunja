648e23e2c0c17e8dbcbb45cb7e47f69d0fc2cefb
kolaente <k@knt.li>
2025-06-24 22:32:23 +0200

    fix: specify table for title search

--- pkg/models/task_search.go
+++ pkg/models/task_search.go
@@ -281,7 +281,7 @@ func (d *dbTaskSearcher) Search(opts *taskSearchOptions) (tasks []*Task, totalCo
 	if opts.search != "" {
 		where =
 			builder.Or(
-				db.ILIKE("title", opts.search),
+				db.ILIKE("tasks.title", opts.search),
 				db.ILIKE("description", opts.search),
 			)
 
