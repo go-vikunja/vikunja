91184e75674625fa04a3486cbdce0d6e7e54a806
kolaente <k@knt.li>
2025-06-25 12:34:36 +0200

    fix: nfpm config

--- build/after-install.sh
+++ build/after-install.sh
@@ -6,4 +6,4 @@ systemctl enable vikunja.service
 NEW_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 sed -i "s/<jwt-secret>/$NEW_SECRET/g" /etc/vikunja/config.yml
 sed -i "s/<rootpath>/\/opt\/vikunja\//g" /etc/vikunja/config.yml
-sed -i "s/Path: \"\.\/vikunja.db\"/Path: \"\\/opt\/vikunja\/vikunja.db\"/g" /etc/vikunja/config.yml
+sed -i "s/path: \"\.\/vikunja.db\"/path: \"\\/opt\/vikunja\/vikunja.db\"/g" /etc/vikunja/config.yml
