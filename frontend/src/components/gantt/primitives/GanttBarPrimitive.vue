bb3c649ee342b2069afd89d29955f9a49a38fb2f
kolaente <k@knt.li>
2025-06-26 14:11:39 +0200

    formatting

--- frontend/src/components/gantt/primitives/GanttBarPrimitive.vue
+++ frontend/src/components/gantt/primitives/GanttBarPrimitive.vue
@@ -27,28 +27,49 @@
 </template>
 
 <script setup lang="ts">
-import {computed,useAttrs} from 'vue'
-import {useGanttBar, type GanttBarModel} from '@/composables/useGanttBar'
-const props = withDefaults(defineProps<{
-  model: GanttBarModel
-  timelineStart: Date
-  timelineEnd: Date
-  onMove: (id: string, newStart: Date, newEnd: Date) => void
-  onDoubleClick?: (model: GanttBarModel) => void
-  as?: string
-}>(), { as: 'div', onDoubleClick: undefined })
+import { computed, useAttrs } from 'vue'
+import { useGanttBar, type GanttBarModel } from '@/composables/useGanttBar'
+
+const props = withDefaults(
+	defineProps<{
+		model: GanttBarModel;
+		timelineStart: Date;
+		timelineEnd: Date;
+		onMove: (id: string, newStart: Date, newEnd: Date) => void;
+		onDoubleClick?: (model: GanttBarModel) => void;
+		as?: string;
+	}>(),
+	{ as: 'div', onDoubleClick: undefined },
+)
 const attrs = useAttrs()
-// eslint-disable-next-line vue/no-setup-props-reactivity-loss
-const {dragging, selected, focused, onPointerDown, onFocus, onBlur, onKeyDown} = useGanttBar({
+ 
+const {
+	dragging,
+	selected,
+	focused,
+	onPointerDown,
+	onFocus,
+	onBlur,
+	onKeyDown,
+	// eslint-disable-next-line vue/no-setup-props-reactivity-loss
+} = useGanttBar({
 	model: props.model,
 	timelineStart: props.timelineStart,
 	timelineEnd: props.timelineEnd,
 	onMove: props.onMove,
 })
-const ariaMin = computed(()=>props.timelineStart.valueOf())
-const ariaMax = computed(()=>props.timelineEnd.valueOf())
-const ariaNow = computed(()=>props.model.start.valueOf())
-const ariaValueText = computed(()=>`${props.model.start.toLocaleString()} – ${props.model.end.toLocaleString()}`)
-const ariaLabel = computed(()=>`Task ${props.model.id}`)
-const dataState = computed(()=>[dragging.value&&'dragging', selected.value&&'selected', focused.value&&'focused'].filter(Boolean).join(' '))
+const ariaMin = computed(() => props.timelineStart.valueOf())
+const ariaMax = computed(() => props.timelineEnd.valueOf())
+const ariaNow = computed(() => props.model.start.valueOf())
+const ariaValueText = computed(() => `${props.model.start.toLocaleString()} – ${props.model.end.toLocaleString()}`)
+const ariaLabel = computed(() => `Task ${props.model.id}`)
+const dataState = computed(() =>
+	[
+		dragging.value && 'dragging',
+		selected.value && 'selected',
+		focused.value && 'focused',
+	]
+		.filter(Boolean)
+		.join(' '),
+)
 </script>
