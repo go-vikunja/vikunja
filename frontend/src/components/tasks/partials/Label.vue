a26fb787f9e9285ea95d66daada995967747d767
kolaente <k@knt.li>
2025-07-15 23:15:09 +0200

    fix(labels): correctly fall back to variable colors when no label color is set (#1124)

--- frontend/src/components/tasks/partials/Label.vue
+++ frontend/src/components/tasks/partials/Label.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 import type {ILabel} from '@/modelTypes/ILabel'
+import {useLabelStyles} from '@/composables/useLabelStyles'
 
 defineProps<{
 	label: ILabel
 }>()
+
+const {getLabelStyles} = useLabelStyles()
 </script>
 
 <template>
 	<span
 		:key="label.id"
-		:style="{'background': label.hexColor, 'color': label.textColor}"
+		:style="getLabelStyles(label)"
 		class="tag"
 	>
 		<span>{{ label.title }}</span>
