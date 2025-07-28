f85c8c62ae36841434ad22ebde6fc5a0ee6ce0d0
kolaente <k@knt.li>
2025-07-28 18:34:26 +0200

    fix: set correct authurl

--- frontend/cypress/e2e/user/openid-login.spec.ts
+++ frontend/cypress/e2e/user/openid-login.spec.ts
@@ -2,7 +2,7 @@ context('OpenID Login', () => {
   it('logs in via Dex provider', () => {
     cy.visit('/login')
     cy.contains('Dex').click()
-    cy.origin('http://localhost:5556', () => {
+    cy.origin('http://dex:5556', () => {
       cy.get('input[name="login"]').type('test')
       cy.get('input[name="password"]').type('12345678')
       cy.get('button[type="submit"]').click()
