7eff9d55139f63e252624ba5e6a4b96a187bb11a
kolaente <k@knt.li>
2025-07-28 20:12:50 +0200

    fix: update ids

--- frontend/cypress/e2e/user/openid-login.spec.ts
+++ frontend/cypress/e2e/user/openid-login.spec.ts
@@ -3,9 +3,9 @@ context('OpenID Login', () => {
     cy.visit('/login')
     cy.contains('Dex').click()
     cy.origin('http://dex:5556', () => {
-      cy.get('input[name="login"]').type('test')
-      cy.get('input[name="password"]').type('12345678')
-      cy.get('button[type="submit"]').click()
+      cy.get('#login').type('test')
+      cy.get('#password').type('12345678')
+      cy.get('#submit-login').click()
     })
     cy.url().should('include', '/')
     cy.get('h2').should('contain', 'Hi test!')
