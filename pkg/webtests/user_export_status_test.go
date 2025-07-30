4639cd6f96d7ec8f4d0e0693f1fcf7f5b795f957
kolaente <k@knt.li>
2025-07-30 17:16:54 +0200

    fix test

--- pkg/webtests/user_export_status_test.go
+++ pkg/webtests/user_export_status_test.go
@@ -20,28 +20,22 @@ import (
 	"net/http"
 	"testing"
 
-	"code.vikunja.io/api/pkg/db"
 	apiv1 "code.vikunja.io/api/pkg/routes/api/v1"
-	"code.vikunja.io/api/pkg/user"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestUserExportStatus(t *testing.T) {
 	t.Run("no export", func(t *testing.T) {
-		rec, err := newTestRequestWithUser(t, http.MethodGet, apiv1.GetUserExportStatus, &testuser1, "", nil, nil)
+		rec, err := newTestRequestWithUser(t, http.MethodGet, apiv1.GetUserExportStatus, &testuser15, "", nil, nil)
 		require.NoError(t, err)
 		assert.Equal(t, "{}\n", rec.Body.String())
 	})
 
 	t.Run("with export", func(t *testing.T) {
-		s := db.NewSession()
-		defer s.Close()
-		_, err := s.Where("id = ?", testuser1.ID).Cols("export_file_id").Update(&user.User{ExportFileID: 1})
-		require.NoError(t, err)
-
 		rec, err := newTestRequestWithUser(t, http.MethodGet, apiv1.GetUserExportStatus, &testuser1, "", nil, nil)
 		require.NoError(t, err)
-		assert.Contains(t, rec.Body.String(), "\"id\":1")
+		assert.Contains(t, rec.Body.String(), `"id":1"`)
 	})
 }
