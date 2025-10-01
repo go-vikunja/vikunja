d97e06b1f7dabe5a9a69945f11e57c386c524a51
Aron <aron.igelstrom@gmail.com>
2025-10-01 16:50:26 +0200

    Restore Link Share Task Creation Permissions

--- pkg/models/users.go
+++ pkg/models/users.go
@@ -24,11 +24,28 @@ import (
 
 // GetUserOrLinkShareUser returns either a user or a link share disguised as a user.
 func GetUserOrLinkShareUser(s *xorm.Session, a web.Auth) (uu *user.User, err error) {
+	// Case 1: The auth object is already a *user.User (could also be a link share proxy with negative ID)
 	if u, is := a.(*user.User); is {
+		// Negative user IDs represent link share principals in the service layer.
+		if u.ID < 0 {
+			shareID := u.ID * -1
+			l, err := GetLinkShareByID(s, shareID)
+			if err != nil {
+				return nil, err
+			}
+			if l == nil {
+				return nil, ErrProjectShareDoesNotExist{ID: shareID}
+			}
+			if NewUserProxyFromLinkShareFunc == nil {
+				panic("NewUserProxyFromLinkShareFunc is not set")
+			}
+			return NewUserProxyFromLinkShareFunc(l), nil
+		}
 		uu, err = user.GetUserByID(s, u.ID)
 		return
 	}
 
+	// Case 2: The auth object is the legacy *LinkSharing instance
 	if ls, is := a.(*LinkSharing); is {
 		l, err := GetLinkShareByID(s, ls.ID)
 		if err != nil {
@@ -40,7 +57,8 @@ func GetUserOrLinkShareUser(s *xorm.Session, a web.Auth) (uu *user.User, err err
 		return NewUserProxyFromLinkShareFunc(l), nil
 	}
 
-	return
+	// Unknown auth type → return nil, nil (caller should treat as unauthenticated)
+	return nil, nil
 }
 
 // NewUserProxyFromLinkShareFunc is a function that returns a user proxy from a link share.
