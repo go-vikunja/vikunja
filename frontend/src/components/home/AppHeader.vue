c63b3550d592b8b303b4cb7a5089799862a37801
kolaente <k@knt.li>
2025-10-08 18:28:40 +0200

    fix(rtl): correct spacing for user avatar menu
    Resolves https://github.com/go-vikunja/vikunja/issues/1544

--- frontend/src/components/home/AppHeader.vue
+++ frontend/src/components/home/AppHeader.vue
@@ -253,6 +253,7 @@ $user-dropdown-width-mobile: 5rem;
 
 .navbar-end {
 	margin-inline-start: 0; // overrides bulma core styles
+	margin-inline-end: 0; // overrides bulma core styles
 	margin-inline-start: auto;
 	flex: 0 0 auto;
 	display: flex;
