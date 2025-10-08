go-vikunja/vikunja

commit c2e224dbb19ece72e72711d069704eefa9eae124

fix: correct case-sensitivity in duedate time parsing (#1613)
Co-authored-by: mechanarchy <1166756+mechanarchy@users.noreply.github.com>

frontend/src/helpers/time/parseDate.ts
@@ -109,7 +109,7 @@ const addTimeToDate = (text: string, date: Date, previousMatch: string | null): 
 		}
 	}
 
-	const timeRegex = ' (at|@) ([0-9][0-9]?(:[0-9][0-9]?)?( ?(a|p)m)?)'
+	const timeRegex = ' (at|@) ([0-9][0-9]?(:[0-9][0-9])?( ?(a|p)m)?)'
 	const matcher = new RegExp(timeRegex, 'ig')
 	const results = matcher.exec(text)
 
@@ -118,7 +118,7 @@ const addTimeToDate = (text: string, date: Date, previousMatch: string | null): 
 		const parts = time.split(':')
 		let hours = parseInt(parts[0])
 		let minutes = 0
-		if (time.endsWith('pm')) {
+		if (time.toLowerCase().endsWith('pm')) {
 			hours += 12
 		}
 		if (parts.length > 1) {
@@ -128,6 +128,7 @@ const addTimeToDate = (text: string, date: Date, previousMatch: string | null): 
 		date.setHours(hours)
 		date.setMinutes(minutes)
 		date.setSeconds(0)
+		date.setMilliseconds(0)
 	}
 
 	const replace = results !== null ? results[0] : previousMatch

frontend/src/modules/parseTaskText.test.ts
@@ -96,6 +96,10 @@ describe('Parse Task Text', () => {
 				'at 3am': '3:0',
 				'at 3:12 am': '3:12',
 				'at 3:12 pm': '15:12',
+				'at 3:12 AM': '3:12',
+				'at 3:12 PM': '15:12',
+				'at 3:12 Am': '3:12',
+				'at 3:12 Pm': '15:12',
 			} as const
 			
 			for (const c in cases) {

commit 1f1e0b227d1a4be2696ed1e9c860b17728bbd424

fix(date parsing): 12pm/12am edge case
Fixes a regression introduced in
c2e224dbb19ece72e72711d069704eefa9eae124

frontend/src/helpers/time/parseDate.ts
@@ -119,7 +119,11 @@ const addTimeToDate = (text: string, date: Date, previousMatch: string | null): 
 		let hours = parseInt(parts[0])
 		let minutes = 0
 		if (time.toLowerCase().endsWith('pm')) {
-			hours += 12
+			if (hours !== 12) {
+				hours += 12
+			}
+		} else if (time.toLowerCase().endsWith('am') && hours === 12) {
+			hours = 0
 		}
 		if (parts.length > 1) {
 			minutes = parseInt(parts[1])

frontend/src/modules/parseTaskText.test.ts
@@ -100,6 +100,8 @@ describe('Parse Task Text', () => {
 				'at 3:12 PM': '15:12',
 				'at 3:12 Am': '3:12',
 				'at 3:12 Pm': '15:12',
+				'at 12:00 pm': '12:0',
+				'at 12:00 am': '0:0',
 			} as const
 			
 			for (const c in cases) {