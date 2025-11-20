1641a9ebb12c4b65e7b8463132cb8715794dd2aa
Mithilesh Gupta <guptamithilesh@protonmail.com>
2025-11-20 17:27:22 +0530

    Registered new route in routes file

--- pkg/models/task_unread_statuses.go
+++ pkg/models/task_unread_statuses.go
@@ -1,8 +1,12 @@
 package models
 
+import "code.vikunja.io/api/pkg/web"
+
 type TaskUnreadStatus struct {
 	TaskID int64 `xorm:"bigint not null unique(task_user)"`
 	UserID int64 `xorm:"bigint not null unique(task_user)"`
+	web.CRUDable
+	web.Permissions
 }
 
 func (TaskUnreadStatus) TableName() string {
