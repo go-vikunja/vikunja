8d468e8d5606b975c702c1bbc1a92d53284fda43
kolaente <k@knt.li>
2026-02-05 23:23:38 +0100

    style(websocket): fix lint issues
    Add license headers, fix unused parameter, use assert.Empty

--- pkg/websocket/messages.go
+++ pkg/websocket/messages.go
@@ -1,3 +1,19 @@
+// Vikunja is a to-do list application to facilitate your life.
+// Copyright 2018-present Vikunja and contributors. All rights reserved.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package websocket
 
 const (
@@ -36,5 +52,5 @@ type OutgoingMessage struct {
 	// Topic identifies the subscription topic (omitted for auth responses).
 	Topic string `json:"topic,omitempty"`
 	// Data carries the event payload.
-	Data interface{} `json:"data,omitempty"`
+	Data any `json:"data,omitempty"`
 }
