46c2bb9e8292f46f334ee1731915028a2a450ab9
kolaente <k@knt.li>
2026-02-05 23:23:38 +0100

    style(websocket): fix lint issues
    Add license headers, fix unused parameter, use assert.Empty

--- pkg/websocket/auth.go
+++ pkg/websocket/auth.go
@@ -1,3 +1,19 @@
+// Vikunja is a to-do list application to facilitate your life.
+// Copyright 2018-present Vikunja and contributors. All rights reserved.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package websocket
 
 import (
@@ -14,7 +30,7 @@ const authTypeLinkShare = 1
 // ValidateToken validates a JWT token and returns the user ID if valid.
 // Returns 0 and an error if the token is invalid.
 func ValidateToken(tokenString string) (int64, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
+	token, err := jwt.Parse(tokenString, func(_ *jwt.Token) (any, error) {
 		return []byte(config.ServiceJWTSecret.GetString()), nil
 	})
 	if err != nil {
