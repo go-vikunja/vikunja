84cf24bd82590e1cd76c85636d6bfc86626f46b7
James Batten <JamesBatten@users.noreply.github.com>
2026-03-02 14:27:40 +0100

    fix(project-views): persist include subprojects per view

--- frontend/src/components/project/views/ProjectKanban.vue
+++ frontend/src/components/project/views/ProjectKanban.vue
@@ -447,6 +447,10 @@ const bucketDraggableComponentData = computed(() => ({
 }))
 const project = computed(() => projectId.value ? projectStore.projects[projectId.value] : null)
 const view = computed(() => project.value?.views.find(v => v.id === props.viewId) as IProjectView || null)
+const requestParams = computed(() => ({
+	...params.value,
+	...(view.value?.includeSubprojects ? {include_subprojects: true} : {}),
+}))
 const canWrite = computed(() => baseStore.currentProject?.maxPermission > Permissions.READ && view.value.bucketConfigurationMode === 'manual')
 const canCreateTasks = computed(() => canWrite.value && projectId.value > 0)
 
@@ -491,7 +495,7 @@ const taskLoading = computed(() => taskStore.isLoading || taskPositionService.va
 
 watch(
 	() => ({
-		params: params.value,
+		params: requestParams.value,
 		projectId: projectId.value,
 		viewId: props.viewId,
 	}),
@@ -526,7 +530,7 @@ function handleTaskContainerScroll(id: IBucket['id'], el: HTMLElement) {
 	kanbanStore.loadNextTasksForBucket(
 		projectId.value,
 		props.viewId,
-		params.value,
+		requestParams.value,
 		id,
 	)
 }
@@ -721,7 +725,7 @@ async function deleteBucket() {
 				projectId: projectIdWithFallback.value,
 				projectViewId: props.viewId,
 			}),
-			params: params.value,
+			params: requestParams.value,
 		})
 		success({message: t('project.kanban.deleteBucketSuccess')})
 	} finally {
@@ -771,7 +775,7 @@ function handleRecurringTaskCompletion() {
 		
 	if (filterContainsDateFields) {
 		// Reload the kanban board to refresh tasks that now match/don't match the filter
-		kanbanStore.loadBucketsForProject(projectId.value, props.viewId, params.value)
+		kanbanStore.loadBucketsForProject(projectId.value, props.viewId, requestParams.value)
 	}
 }
 
