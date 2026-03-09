dde01a432765dbda9e12898c08ad3fb8bc66b454
kolaente <k@knt.li>
2026-03-09 13:30:23 +0100

    fix: prefer working directory for service.rootpath default
    When running as a systemd service, the binary is often in /usr/local/bin
    but WorkingDirectory points to a data directory like /var/lib/vikunja.
    Previously, rootpath defaulted to the binary's directory, causing
    Vikunja to attempt creating files/db in /usr/local/bin.
    
    Now os.Getwd() is preferred, which respects systemd's WorkingDirectory
    and is the intuitive default for all deployment scenarios.

---
+++ pkg/config/config_test.go
@@ -0,0 +1,34 @@
+// Vikunja is a to-do list application to facilitate your life.
+// Copyright 2018-present Vikunja and contributors. All rights reserved.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package config
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetRootpathLocation(t *testing.T) {
+	// The function should return the current working directory
+	expected, err := os.Getwd()
+	require.NoError(t, err)
+
+	result := getRootpathLocation()
+	assert.Equal(t, expected, result)
+}
