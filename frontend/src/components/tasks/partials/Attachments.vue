c48746da87fe6431fb22c607528f7b462917be98
kolaente <hello@kolaente.de>
2026-04-04 21:25:16 +0200

    fix: height

--- frontend/src/components/tasks/partials/Attachments.vue
+++ frontend/src/components/tasks/partials/Attachments.vue
@@ -595,7 +595,7 @@ defineExpose({
 .pdf-preview-iframe {
 	inline-size: 100%;
 	max-inline-size: calc(100% - 4rem);
-	block-size: 100vh;
+	block-size: calc(100vh - var(--modal-content-spacing-tablet));
 	border: none;
 	margin: 0 auto;
 	display: block;
