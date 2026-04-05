05acc2b6609b84765e9f706152f9cda1bbc47e92
kolaente <k@knt.li>
2026-04-05 20:00:15 +0200

    feat(api): bot token support via /tokens CRUD and bot_users_enabled flag

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -493,10 +493,6 @@ func registerAPIRoutes(a *echo.Group) {
 		u.GET("/bots/:bot", botHandler.ReadOneWeb)
 		u.POST("/bots/:bot", botHandler.UpdateWeb)
 		u.DELETE("/bots/:bot", botHandler.DeleteWeb)
-
-		u.PUT("/bots/:bot/tokens", apiv1.CreateBotToken)
-		u.GET("/bots/:bot/tokens", apiv1.ListBotTokens)
-		u.DELETE("/bots/:bot/tokens/:token", apiv1.DeleteBotToken)
 	}
 
 	projectHandler := &handler.WebHandler{
