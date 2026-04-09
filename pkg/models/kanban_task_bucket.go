91fa5bfb3498e4b9a65aa0795d28590d7b141f07
kolaente <k@knt.li>
2026-04-09 11:58:46 +0200

    fix(kanban): route repeating tasks to default bucket when dropped on done (#2573)

--- pkg/models/kanban_task_bucket.go
+++ pkg/models/kanban_task_bucket.go
@@ -145,8 +145,13 @@ func updateTaskBucket(s *xorm.Session, a web.Auth, b *TaskBucket) (err error) {
 				oldTask := *task
 				oldTask.Done = false
 				updateDone(&oldTask, task)
-				updateBucket = false
-				b.BucketID = oldTaskBucket.BucketID
+				// A repeating task doesn't stay in the done bucket; route
+				// it back to the view's default bucket so the user sees
+				// the next iteration waiting in the "To-Do" column.
+				b.BucketID, err = getDefaultBucketID(s, view)
+				if err != nil {
+					return err
+				}
 			}
 		}
 
