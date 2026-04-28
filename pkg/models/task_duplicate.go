4649b1e60983ff5c3d13ca7ed470576e6a383a8e
iamsamuelrodda <iamsamuelrodda@users.noreply.github.com>
2026-04-28 21:21:52 +0930

    feat(repeat): migrate from legacy repeat fields to RFC 5545 RRULE

--- pkg/models/task_duplicate.go
+++ pkg/models/task_duplicate.go
@@ -74,20 +74,21 @@ func (td *TaskDuplicate) Create(s *xorm.Session, doer web.Auth) (err error) {
 
 	// Create the new task
 	newTask := &Task{
-		Title:       originalTask.Title,
-		Description: originalTask.Description,
-		Done:        false,
-		DueDate:     originalTask.DueDate,
-		ProjectID:   originalTask.ProjectID,
-		RepeatAfter: originalTask.RepeatAfter,
-		RepeatMode:  originalTask.RepeatMode,
-		Priority:    originalTask.Priority,
-		StartDate:   originalTask.StartDate,
-		EndDate:     originalTask.EndDate,
-		HexColor:    originalTask.HexColor,
-		PercentDone: originalTask.PercentDone,
-		Assignees:   originalTask.Assignees,
-		Reminders:   originalTask.Reminders,
+		Title:                  originalTask.Title,
+		Description:            originalTask.Description,
+		Done:                   false,
+		DueDate:                originalTask.DueDate,
+		ProjectID:              originalTask.ProjectID,
+		Repeats:                originalTask.Repeats,
+		Repeat:                 originalTask.Repeat,
+		RepeatsFromCurrentDate: originalTask.RepeatsFromCurrentDate,
+		Priority:               originalTask.Priority,
+		StartDate:              originalTask.StartDate,
+		EndDate:                originalTask.EndDate,
+		HexColor:               originalTask.HexColor,
+		PercentDone:            originalTask.PercentDone,
+		Assignees:              originalTask.Assignees,
+		Reminders:              originalTask.Reminders,
 	}
 
 	err = createTask(s, newTask, doer, true, true)
