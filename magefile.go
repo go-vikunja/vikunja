8d6e0e55fcf82f8ab883877c4d003e6f0eac29e4
kolaente <k@knt.li>
2026-08-20 09:53:22 +0200

    chore: bump golangci-lint to v2.13.0
    Disables staticcheck SA4023: the nilness analyzer vendored in 2.13.0
    (honnef.co/go/tools v0.8.0-rc.1) panics on recover() in pointer-returning
    functions, triggered by sentry-go (dominikh/go-tools#1725).

--- magefile.go
+++ magefile.go
@@ -1097,7 +1097,7 @@ func extractFrontendTranslationKeysFromFile(filePath string) ([]TranslationKey, 
 func checkGolangCiLintInstalled(ctx context.Context) error {
 	mg.Deps(initVars, ensureFrontendDistExists)
 	if err := exec.CommandContext(ctx, "golangci-lint").Run(); err != nil && strings.Contains(err.Error(), "executable file not found") {
-		return fmt.Errorf("golangci-lint executable failed to run, please manually install golangci-lint by running the command: curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.4.0")
+		return fmt.Errorf("golangci-lint executable failed to run, please manually install golangci-lint by running the command: curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.13.0")
 	}
 	return nil
 }
