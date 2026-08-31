cdfd3ffc8d4d5ac3c72d5829ae2515ef3779d1ff
kolaente <k@knt.li>
2026-09-01 00:34:17 +0200

    fix(frontend): guard failed project detail loads

--- frontend/src/views/project/settings/ProjectSettingsEdit.vue
+++ frontend/src/views/project/settings/ProjectSettingsEdit.vue
@@ -4,54 +4,58 @@
 		:title="$t('project.edit.header')"
 		primary-icon=""
 		:primary-label="$t('misc.save')"
+		:primary-disabled="Boolean(loadError)"
 		:tertiary="project.max_permission === PERMISSIONS.ADMIN ? $t('misc.delete') : undefined"
 		@primary="save"
 		@tertiary="$router.push({ name: 'project.settings.delete', params: { id: projectId } })"
 	>
-		<FormField
-			id="title"
-			v-model="project.title"
-			v-focus
-			:label="$t('project.title')"
-			:disabled="isLoading"
-			:placeholder="$t('project.edit.titlePlaceholder')"
-			type="text"
-			@keyup.enter="save"
-		/>
-		<FormField :label="$t('project.parent')">
-			<ProjectSearch v-model="parentProject" />
-		</FormField>
-		<FormField :label="$t('project.edit.description')">
-			<Editor
-				id="projectdescription"
-				v-model="project.description"
-				:class="{ 'disabled': isLoading}"
+		<ErrorMessage v-if="loadError" />
+		<template v-else>
+			<FormField
+				id="title"
+				v-model="project.title"
+				v-focus
+				:label="$t('project.title')"
 				:disabled="isLoading"
-				:placeholder="$t('project.edit.descriptionPlaceholder')"
+				:placeholder="$t('project.edit.titlePlaceholder')"
+				type="text"
+				@keyup.enter="save"
 			/>
-		</FormField>
-
-		<div class="columns">
-			<div class="column">
-				<FormField
-					id="identifier"
-					v-model="project.identifier"
-					v-tooltip="$t('project.edit.identifierTooltip')"
-					:label="$t('project.edit.identifier')"
+			<FormField :label="$t('project.parent')">
+				<ProjectSearch v-model="parentProject" />
+			</FormField>
+			<FormField :label="$t('project.edit.description')">
+				<Editor
+					id="projectdescription"
+					v-model="project.description"
+					:class="{ 'disabled': isLoading}"
 					:disabled="isLoading"
-					:placeholder="$t('project.edit.identifierPlaceholder')"
-					type="text"
-					maxlength="10"
-					@keyup.enter="save"
+					:placeholder="$t('project.edit.descriptionPlaceholder')"
 				/>
-			</div>
+			</FormField>
 
-			<div class="column">
-				<FormField :label="$t('project.edit.color')">
-					<ColorPicker v-model="project.hex_color" />
-				</FormField>
+			<div class="columns">
+				<div class="column">
+					<FormField
+						id="identifier"
+						v-model="project.identifier"
+						v-tooltip="$t('project.edit.identifierTooltip')"
+						:label="$t('project.edit.identifier')"
+						:disabled="isLoading"
+						:placeholder="$t('project.edit.identifierPlaceholder')"
+						type="text"
+						maxlength="10"
+						@keyup.enter="save"
+					/>
+				</div>
+
+				<div class="column">
+					<FormField :label="$t('project.edit.color')">
+						<ColorPicker v-model="project.hex_color" />
+					</FormField>
+				</div>
 			</div>
-		</div>
+		</template>
 	</CreateEdit>
 </template>
 
@@ -65,6 +69,7 @@ import ColorPicker from '@/components/input/ColorPicker.vue'
 import CreateEdit from '@/components/misc/CreateEdit.vue'
 import FormField from '@/components/input/FormField.vue'
 import ProjectSearch from '@/components/tasks/partials/ProjectSearch.vue'
+import ErrorMessage from '@/components/misc/Error.vue'
 
 import type {ProjectResponse} from '@/client/queries/projects'
 
@@ -86,7 +91,7 @@ const projectStore = useProjectNavigation()
 
 const {t} = useI18n({useScope: 'global'})
 
-const {project, save: saveProject, isLoading} = useProject(() => props.projectId)
+const {project, save: saveProject, isLoading, error: loadError} = useProject(() => props.projectId)
 
 const parentProject = ref<ProjectResponse | null>(null)
 const isSaving = ref(false)
