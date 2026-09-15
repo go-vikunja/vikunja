0ca54e04effa0da6909337d8b3b7d0d6fa832e8b
kolaente <k@knt.li>
2026-09-15 13:53:26 +0200

    feat: add /.well-known/change-password redirect
    Implements https://www.w3.org/TR/change-password-url/ so password managers can link users to the password settings page.

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -250,6 +250,8 @@ func RegisterRoutes(e *echo.Echo) {
 	f.Use(middleware.BasicAuth(feeds.BasicAuth))
 	f.GET("/notifications.atom", feeds.NotificationsAtomFeed)
 
+	e.GET("/.well-known/change-password", ChangePasswordRedirect)
+
 	// healthcheck
 	e.GET("/health", HealthcheckHandler)
 
