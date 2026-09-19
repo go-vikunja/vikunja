c94247588f105bb54b7b8dd5e779c62139666e4c
kolaente <k@knt.li>
2026-09-19 15:42:39 +0200

    fixup! refactor(subscriptions): switch to generated fields (mechanical)
    address pr-swarm finding: typed the entity prop as NonNullable<Subscription['entity']>, dropped the unreachable '' fallback (switch turned into if/return because vue/return-in-computed-property is not type-aware) and un-aliased the generated Subscription import

--- frontend/src/components/misc/Subscription.vue
+++ frontend/src/components/misc/Subscription.vue
@@ -24,13 +24,13 @@ import {useI18n} from 'vue-i18n'
 
 import DropdownItem from '@/components/misc/DropdownItem.vue'
 
-import type {Subscription as ISubscription} from '@/client/generated'
+import type {Subscription} from '@/client/generated'
 
 import type { IconProp } from '@fortawesome/fontawesome-svg-core'
 
 const props = withDefaults(defineProps<{
-	modelValue: ISubscription | null,
-	entity: ISubscription['entity'],
+	modelValue: Subscription | null,
+	entity: NonNullable<Subscription['entity']>,
 	entityId: number,
 	type?: 'button' | 'dropdown',
 }>(), {
@@ -53,18 +53,15 @@ const tooltipText = computed(() => {
 			: t('task.subscription.subscribedProjectThroughParentProject')
 	}
 
-	switch (props.entity) {
-		case 'project':
-			return props.modelValue !== null ?
-				t('task.subscription.subscribedProject') :
-				t('task.subscription.notSubscribedProject')
-		case 'task':
-			return props.modelValue !== null ?
-				t('task.subscription.subscribedTask') :
-				t('task.subscription.notSubscribedTask')
+	if (props.entity === 'task') {
+		return props.modelValue !== null
+			? t('task.subscription.subscribedTask')
+			: t('task.subscription.notSubscribedTask')
 	}
 
-	return ''
+	return props.modelValue !== null
+		? t('task.subscription.subscribedProject')
+		: t('task.subscription.notSubscribedProject')
 })
 
 const buttonText = computed(() => props.modelValue ? t('task.subscription.unsubscribe') : t('task.subscription.subscribe'))
