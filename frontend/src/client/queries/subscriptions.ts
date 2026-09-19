b503ab225efeb9a8b1a2f1bba5bbc29dfbf0d733
kolaente <k@knt.li>
2026-09-19 10:06:18 +0200

    refactor(subscriptions): move task subscriptions into query mutations

---
+++ frontend/src/client/queries/subscriptions.ts
@@ -0,0 +1,32 @@
+import {useMutation} from '@tanstack/vue-query'
+import {subscriptionsCreate, subscriptionsDelete} from '@/client/generated'
+import {contextMutationOptions} from './contextMutation'
+import {mapTaskEverywhere} from './taskCache'
+import {taskKeys} from './tasks'
+import {i18n} from '@/i18n'
+
+export function setTaskSubscriptionMutationOptions() {
+	return contextMutationOptions({
+		mutationFn: async ({taskId, subscribed}: {taskId: number, subscribed: boolean}) => {
+			const path = {entity: 'task', entityID: taskId} as const
+			if (!subscribed) {
+				await subscriptionsDelete({path})
+				return undefined
+			}
+			return (await subscriptionsCreate({path})).data
+		},
+		onSuccess: (subscription, {taskId}, client) => {
+			mapTaskEverywhere(client, taskId, task => ({...task, subscription}))
+		},
+		onSettled: ({taskId}, client) => Promise.all([
+			client.invalidateQueries({queryKey: taskKeys.detail(taskId)}),
+			client.invalidateQueries({queryKey: taskKeys.all, refetchType: 'none'}),
+		]),
+		successMessage: (_data, {subscribed}) => i18n.global.t(subscribed
+			? 'task.subscription.subscribeSuccessTask' : 'task.subscription.unsubscribeSuccessTask'),
+	})
+}
+
+export function useSetTaskSubscriptionMutation() {
+	return useMutation(setTaskSubscriptionMutationOptions())
+}
