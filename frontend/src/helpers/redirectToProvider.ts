275075ff437a46d5e755fda10906b4cb1c5a5210
kolaente <k@knt.li>
2026-09-21 22:39:35 +0200

    fixup! refactor(config): switch consumers to wire fields (mechanical)
    address pr-swarm finding: dropped dead scope fallback in redirectToProvider, inlined provider.scope

--- frontend/src/helpers/redirectToProvider.ts
+++ frontend/src/helpers/redirectToProvider.ts
@@ -17,11 +17,7 @@ export const redirectToProvider = (provider: IProvider) => {
 	const state = createRandomID(24)
 	localStorage.setItem('state', state)
 
-	let scope = 'openid email profile'
-	if (provider.scope !== null){
-		scope = provider.scope
-	}
-	window.location.href = `${provider.auth_url}?client_id=${provider.client_id}&redirect_uri=${redirectUrl}&response_type=code&scope=${scope}&state=${state}`
+	window.location.href = `${provider.auth_url}?client_id=${provider.client_id}&redirect_uri=${redirectUrl}&response_type=code&scope=${provider.scope}&state=${state}`
 }
 
 export const redirectToProviderOnLogout = (provider: IProvider): boolean => {
