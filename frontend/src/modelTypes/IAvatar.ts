2efef28e2cdf57db3c0be9a23602879302a52250
kolaente <k@knt.li>
2026-09-21 18:49:57 +0200

    refactor(avatar): delete the legacy avatar stack

--- frontend/src/modelTypes/IAvatar.ts
+++
@@ -1,7 +0,0 @@
-import type {IAbstract} from './IAbstract'
-
-export type AvatarProvider = 'default' | 'initials' | 'gravatar' | 'marble' | 'upload'
-
-export interface IAvatar extends IAbstract {
-	avatarProvider: AvatarProvider
-}
