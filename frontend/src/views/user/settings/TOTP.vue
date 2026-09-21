a6e5049a65d6f7892ccbb7effdc240da46f58b25
kolaente <k@knt.li>
2026-09-21 22:48:24 +0200

    fixup! refactor(totp): subscribe to status and scope QR object URLs
    address pr-swarm finding: Confirm/Disable buttons bind isPending and their handlers early-return while pending

--- frontend/src/views/user/settings/TOTP.vue
+++ frontend/src/views/user/settings/TOTP.vue
@@ -36,7 +36,10 @@
 				inputmode="numeric"
 				@keyup.enter="totpConfirm"
 			/>
-			<XButton @click="totpConfirm">
+			<XButton
+				:loading="enableMutation.isPending.value"
+				@click="totpConfirm"
+			>
 				{{ $t('misc.confirm') }}
 			</XButton>
 		</template>
@@ -64,6 +67,7 @@
 				/>
 				<XButton
 					danger
+					:loading="disableMutation.isPending.value"
 					@click="totpDisable"
 				>
 					{{ $t('user.settings.totp.disable') }}
@@ -121,6 +125,7 @@ const enableMutation = useEnableTotpMutation()
 const disableMutation = useDisableTotpMutation()
 
 async function totpConfirm() {
+	if (enableMutation.isPending.value) return
 	try {
 		await enableMutation.mutateAsync(totpConfirmPasscode.value)
 		await authStore.logout()
@@ -133,6 +138,7 @@ function closeDisableForm() {
 }
 
 async function totpDisable() {
+	if (disableMutation.isPending.value) return
 	try {
 		await disableMutation.mutateAsync(totpDisablePassword.value)
 	} catch {
